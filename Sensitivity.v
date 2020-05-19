1450ca14a84d308f9eb38541767c947021b5c856
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-05-19 18:09:39 +0200

    -

--- Sensitivity.v
+++ Sensitivity.v
@@ -2215,6 +2215,21 @@ Compute (let (n, i) := (3, 5) in map (λ k, (matel (A n) i k * matel (A n) k i)%
 Compute (let '(i, j, n) := (1, 2, 3) in map (λ k, (matel (A n) i k * matel (A n) k j)%Rng) (seq 0 8)).
 Compute (let '(i, j, n) := (2, 1, 3) in map (λ k, (matel (A n) i k * matel (A n) k j)%Rng) (seq 0 8)).
 Compute (let '(i, j, n) := (0, 1, 3) in map (λ k, (matel (A n) i k * matel (A n) k j)%Rng) (seq 0 8)).
+    replace (Σ (_ = _, _), _)%Rng with
+      (Σ (k = 0, 2 ^ n - 1),
+       (matel (A n) i k * matel (A n) j k +
+        (if Nat.eq_dec i k then 1%Z else 0%Z) *
+        (if Nat.eq_dec j k then 1%Z else 0%Z)))%Rng. 2: {
+      apply summation_compat.
+      intros k Hk.
+      rewrite (A_symm _ j); f_equal; f_equal.
+      destruct (Nat.eq_dec j k) as [Hjk| Hjk]. {
+        symmetry in Hjk.
+        now destruct (Nat.eq_dec k j).
+      }
+      destruct (Nat.eq_dec k j) as [H| H]; [ | easy ].
+      now symmetry in H.
+    }
 ...
     destruct (lt_dec i j) as [Hilj| H]. {
       rewrite (summation_split i); [ | flia Hin ].
