c31122706ae43775a8ca8b2da05e06838862e5f6
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-06-02 19:25:15 +0200

    -

--- Sensitivity.v
+++ Sensitivity.v
@@ -2445,8 +2445,7 @@ Fixpoint mmat_ncols {T} {ro : ring_op T} (MM : mmatrix T) :=
   | MM_M _ nc mm => Σ (j = 0, nc - 1), mmat_ncols (matel mm 0 j)
   end.
 
-...
-
+(*
 Fixpoint mmat_which_row {T} nr (mm : matrix (mmatrix T)) (i im : nat) :=
   match nr with
   | 0 => (0, 0) (* should not happen *)
@@ -2455,7 +2454,6 @@ Fixpoint mmat_which_row {T} nr (mm : matrix (mmatrix T)) (i im : nat) :=
       | MM_1 nr' _ M =>
           if lt_dec i nr' then im
           else mmat_which_row mm ...
-...
 
 Definition mmat_which_col {T} (nc : nat) (mm : matrix (mmatrix T)) (j : nat) := (0, 0).
 
@@ -2468,8 +2466,7 @@ Fixpoint mmatel' {T} {ro : ring_op T} (MM : mmatrix T) i j :=
       mmatel' (matel mm im jm) (i - nrows_bef) (j - ncols_bef)
   end.
 
-...
-(**)
+*)
 
 Compute (let n := 1 in list_of_mat (2 ^ n) (2 ^ n) (let _ := Z_ring_op in A n)).
 Compute (let n := 1 in list_of_mmat 0%Z (let _ := Z_ring_op in A' n)).
