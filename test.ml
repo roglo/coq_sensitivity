753669bbf124b340b73d21326fb3a34bdd1cdbbf
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-06-24 08:59:34 +0200

    -

--- test.ml
+++ test.ml
@@ -461,10 +461,10 @@ let n = 3 in mA int_ring_op n;
 let n = 3 in mmat_mul nat_semiring_op (mA int_ring_op n) (mA int_ring_op n);
 44;
 
-value rec mat_of_mmat mm =
+value rec list_list_of_mmat mm =
   match mm with
   | MM_1 m → List.concat m.mat_list
   | MM_M mmm →
-      List.concat (List.concat (map (map mat_of_mmat) mmm.mat_list))
+      List.concat (List.concat (map (map list_list_of_mmat) mmm.mat_list))
   end.
-mat_of_mmat (mA int_ring_op 2);
+list_list_of_mmat (mA int_ring_op 2);
