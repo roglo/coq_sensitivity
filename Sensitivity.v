c097e55b64656eb4d89e69028016df5821f76e99
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-08-05 15:53:54 +0200

    -

--- Sensitivity.v
+++ Sensitivity.v
@@ -3160,19 +3160,19 @@ f_equal. {
   induction la as [| a]; intros; [ easy | ].
   destruct lb as [| b]; [ easy | ].
   cbn in Hd |-*.
-  remember (fold_left max (map (bmat_depth (T:=T)) la) (bmat_depth a)) as k
-    eqn:Hk.
-  assert (Hki : bmat_depth a ≤ k). {
-    subst k; clear.
-    remember (bmat_depth a) as k; clear Heqk.
-    revert k.
-    induction la as [| a1]; intros; [ easy | cbn ].
-    etransitivity; [ | apply IHla ].
-    apply Nat.le_max_l.
-  }
-  clear Hk.
   f_equal. {
     symmetry.
+    remember (fold_left max (map (bmat_depth (T:=T)) la) (bmat_depth a)) as k
+                                                                              eqn:Hk.
+    assert (Hki : bmat_depth a ≤ k). {
+      subst k; clear.
+      remember (bmat_depth a) as k; clear Heqk.
+      revert k.
+      induction la as [| a1]; intros; [ easy | cbn ].
+      etransitivity; [ | apply IHla ].
+      apply Nat.le_max_l.
+    }
+    clear Hk.
     rewrite (IHMa (a :: la)); [ | now left | now left | ]. {
       symmetry.
       apply (IHMa (a :: la)); [ now left | now left | ].
@@ -3202,10 +3202,10 @@ f_equal. {
     transitivity k; [ easy | ].
     apply Nat.le_max_l.
   }
-  rewrite IHla. {
-    apply list_add_add_compat.
-    intros Ma Mb.
-...
+  apply list_add_add_compat.
+  intros Ma Mb.
+  remember (fold_left _ _ _) as ita' eqn:Hita' in Hd.
+  rewrite <- Hita'.
 Theorem bmat_def_add_loop_enough_iter : ∀ T (add : T → T → T) ita Ma Mb,
   bmat_depth Ma ≤ ita
   → bmat_def_add_loop add ita Ma Mb = bmat_def_add add Ma Mb.
