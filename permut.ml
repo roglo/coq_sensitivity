09672e26e64bd8e12d59354f0e5db9469b6d5be9
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-10-05 08:50:56 +0200

    -

--- permut.ml
+++ permut.ml
@@ -153,8 +153,7 @@ value rec insert n l p =
 value rec distrib n ppl =
   match ppl with
   | [] -> []
-  | [((l, p) : (list α * bool)) :: ppl] ->
-       List.append (insert n l p) (distrib n ppl)
+  | [((l, p) :: ppl] -> List.append (insert n l p) (distrib n ppl)
   end.
 
 value rec all_permut_and_parity list =
