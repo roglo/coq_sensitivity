8263f170c59b31594e56a562d9aa35cd3f9bbede
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-10-29 08:50:52 +0100

    -

--- Lemma_2_2.v
+++ Lemma_2_2.v
@@ -5,8 +5,8 @@ Set Nested Proofs Allowed.
 Set Implicit Arguments.
 
 Require Import Utf8 Arith.
-(*
 Import List List.ListNotations.
+(*
 Require Import Init.Nat.
 Require Import Relations.
 *)
@@ -30,6 +30,111 @@ Add Parametric Relation : _ (@bmat_fit_for_add T)
  symmetry proved by bmat_fit_for_add_symm
  transitivity proved by bmat_fit_for_add_trans
  as bmat_fit_for_add_equivalence.
+
+(* sequence "An" *)
+
+Fixpoint A n : bmatrix T :=
+  match n with
+  | 0 => BM_1 0%Srng
+  | S n' =>
+       BM_M
+         (mat_of_list_list (BM_1 0%Srng)
+            [[A n'; I_2_pow n'];
+             [I_2_pow n'; bmat_opp (A n')]])
+  end.
+
+Theorem bmat_fit_for_add_IZ_A : ∀ u n,
+  bmat_fit_for_add (IZ_2_pow u n) (A n).
+Proof.
+intros.
+revert u.
+induction n; intros; [ easy | cbn ].
+split; [ easy | ].
+split; [ easy | ].
+intros * Hi Hj.
+destruct i. {
+  destruct j; [ apply IHn | ].
+  destruct j; [ cbn | flia Hj ].
+  apply bmat_fit_for_add_IZ_IZ.
+}
+destruct i; [ | flia Hi ].
+destruct j; [ apply bmat_fit_for_add_IZ_IZ | ].
+destruct j; [ cbn | flia Hj ].
+transitivity (A n); [ easy | ].
+apply bmat_fit_for_add_opp_r.
+Qed.
+
+Theorem sizes_of_bmatrix_A : ∀ n,
+  sizes_of_bmatrix (A n) = repeat 2 n.
+Proof.
+intros.
+induction n; [ easy | now cbn; f_equal ].
+Qed.
+
+Theorem A_is_square_bmat : ∀ n,
+  is_square_bmat (A n).
+Proof.
+intros.
+induction n; [ easy | cbn ].
+split; [ easy | ].
+split; [ easy | ].
+intros i j Hi Hj.
+destruct i; cbn. {
+  destruct j; [ easy | cbn ].
+  destruct j; [ | flia Hj ].
+  rewrite sizes_of_bmatrix_A.
+  rewrite <- (sizes_of_bmatrix_IZ n 1%Srng).
+  apply IZ_is_square_bmat.
+}
+destruct i; [ cbn | flia Hi ].
+destruct j; cbn. {
+  rewrite sizes_of_bmatrix_A.
+  rewrite <- (sizes_of_bmatrix_IZ n 1%Srng).
+  apply IZ_is_square_bmat.
+}
+destruct j; [ | flia Hj ].
+apply is_square_bmat_loop_opp.
+apply IHn.
+Qed.
+
+Theorem bmat_zero_like_A_eq_Z :
+  ∀ n, bmat_zero_like (A n) = Z_2_pow n.
+Proof.
+intros.
+induction n; [ easy | cbn ].
+f_equal.
+apply matrix_eq; cbn; [ easy | easy | ].
+intros i j Hi Hj.
+destruct i; cbn. {
+  destruct j; cbn; [ easy | ].
+  destruct j; [ cbn | flia Hj ].
+  apply bmat_zero_like_IZ_eq_Z.
+}
+destruct i; [ cbn | flia Hi ].
+destruct j; cbn. {
+  apply bmat_zero_like_IZ_eq_Z.
+}
+destruct j; [ | flia Hj ].
+unfold so.
+rewrite bmat_zero_like_opp; [ easy | ].
+apply A_is_square_bmat.
+Qed.
+
+Theorem Tr_A : ∀ n, Tr (A n) = 0%Srng.
+Proof.
+intros.
+(*
+revert n.
+apply nat_ind; [ | intros n IHn ]; [ easy | ].
+*)
+induction n; [ easy | cbn ].
+rewrite IHn.
+do 2 rewrite srng_add_0_l.
+unfold so.
+rewrite Tr_opp; [ | apply A_is_square_bmat ].
+rewrite IHn.
+apply rng_opp_0.
+Qed.
 
 (* "We prove by induction that A_n^2 = nI" *)
 
