99400944ae67d4410e347d44bcf3e1cfc40ac803
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-10-29 07:45:50 +0100

    -

---
+++ Lemma_2_2.v
@@ -0,0 +1,109 @@
+(* sequence A_n of matrices defined in sensitivity conjecture
+   and the proof their square is "n * I_n" *)
+
+Set Nested Proofs Allowed.
+Set Implicit Arguments.
+
+Require Import Utf8 Arith.
+(*
+Import List List.ListNotations.
+Require Import Init.Nat.
+Require Import Relations.
+*)
+
+Require Import Misc Matrix.
+Require Import Semiring (*SRsummation*).
+Require Import BlockMat.
+Import bmatrix_Notations.
+
+Section in_ring.
+
+Context {T : Type}.
+Context {ro : ring_op T}.
+Context (so := rng_semiring).
+Context {sp : @semiring_prop T (@rng_semiring T ro)}.
+Context {rp : @ring_prop T ro}.
+Existing Instance so.
+
+(* "We prove by induction that A_n^2 = nI" *)
+
+Theorem lemma_2_A_n_2_eq_n_I : ∀ n,
+  (A n * A n = bmat_nat_mul_l n (I_2_pow n))%BM.
+Proof.
+intros.
+induction n; intros; [ now cbn; rewrite srng_mul_0_l | ].
+cbn; f_equal.
+apply matrix_eq; cbn; [ easy | easy | ].
+intros * Hi Hj.
+destruct i. {
+  destruct j. {
+    cbn.
+    rewrite bmat_nat_mul_l_succ.
+    rewrite <- IHn.
+    unfold so.
+Check bmat_mul_1_r.
+    rewrite bmat_mul_1_r; [ | ].
+(* Ah oui, faut peut-être être sûr que bmat_fit_for_add est bien
+   une relation d'équivalence ; à importer de BlockMat d'une manière
+   ou d'une autre *)
+2: unfold I_2_pow; cbn.
+Print bmat_fit_for_add.
+...
+    rewrite bmat_mul_1_r; [ | easy ].
+    f_equal.
+    rewrite <- bmat_zero_like_sqr; [ | apply A_is_square_bmat ].
+    apply bmat_add_0_l.
+  }
+  destruct j; [ cbn | flia Hj ].
+  rewrite bmat_nat_mul_l_succ.
+  rewrite bmat_mul_1_r. 2: {
+    unfold I_2_pow.
+    apply bmat_fit_for_add_IZ_A.
+  }
+  rewrite bmat_mul_1_l. 2: {
+    unfold I_2_pow.
+    transitivity (A n); [ apply bmat_fit_for_add_IZ_A | ].
+    apply bmat_fit_for_add_opp_r.
+  }
+  rewrite bmat_add_0_l.
+  rewrite bmat_add_opp_r.
+  rewrite fold_Z_2_pow.
+  rewrite old_bmat_add_0_r. 2: {
+    now apply bmat_fit_for_add_Z_2_pow_bmat_nat_mul_l.
+  }
+  rewrite bmat_nat_mul_0_r.
+  now apply bmat_zero_like_A_eq_Z.
+}
+destruct i; [ | flia Hi ].
+destruct j; cbn. {
+  rewrite bmat_mul_1_l. 2: {
+    apply bmat_fit_for_add_IZ_A.
+  }
+  rewrite bmat_mul_1_r. 2: {
+    transitivity (A n); [ | apply bmat_fit_for_add_opp_r ].
+    apply bmat_fit_for_add_IZ_A.
+  }
+  rewrite bmat_add_0_l.
+  rewrite bmat_add_opp_r.
+  rewrite bmat_nat_mul_l_succ.
+  rewrite fold_Z_2_pow.
+  rewrite bmat_nat_mul_0_r.
+  rewrite old_bmat_add_0_r; [ | easy ].
+  now apply bmat_zero_like_A_eq_Z.
+}
+destruct j; [ cbn | flia Hj ].
+rewrite bmat_mul_1_l; [ | easy ].
+rewrite bmat_mul_sqr_opp; [ | apply A_is_square_bmat ].
+rewrite bmat_nat_mul_l_succ.
+rewrite <- IHn.
+rewrite bmat_zero_like_A_eq_Z.
+rewrite old_bmat_add_0_l; [ | apply bmat_fit_for_add_IZ_IZ ].
+apply bmat_add_comm.
+transitivity (A n). 2: {
+  apply (is_square_bmat_fit_for_add (sizes_of_bmatrix (A n))). {
+    apply A_is_square_bmat.
+  }
+  apply is_square_bmat_loop_mul; apply A_is_square_bmat.
+}
+apply bmat_fit_for_add_IZ_A.
+Qed.
