79b88459ae790d55697af05047f25c8ec3d13487
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-11-17 18:25:44 +0100

    -

--- Matrix.v
+++ Matrix.v
@@ -357,8 +357,8 @@ cbn - [ iter_seq ].
 (**)
 destruct n; [ flia Hiz | ].
 cbn - [ iter_seq ].
-Abort. (* blocked by the present implementation of discriminant
 ...
+(* blocked by the present implementation of discriminant
 erewrite srng_summation_eq_compat; [ | easy | ]. 2: {  
   intros j Hj.
   rewrite (srng_summation_split _ (i - 1)); [ | flia Hiz ].
