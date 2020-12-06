74d010bf7f6d7c29c87d6d48586b10321e02b532
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-12-06 22:29:53 +0100

    -

--- Semiring.v
+++ Semiring.v
@@ -1,265 +1,291 @@
-(* Rings *)
-(* the name of this module should be changed
-   because here, a semiring is a particular
-   case of rings where the opposite has no
-   property *)
+(* Ring-like *)
+(* Algebraic structure with two operations *)
+(* Allows to define semirings, rings, fields, commutative or not,
+   with two structures:
+     ring_like_op, holding the operations, and
+     ring_like_prop, holding their properties *)
 
 Require Import Utf8.
 
-Class ring_op A :=
-  { rng_zero : A;
-    rng_one : A;
-    rng_add : A → A → A;
-    rng_mul : A → A → A;
-    rng_opp : A → A }.
+Class ring_like_op T :=
+  { rngl_zero : T;
+    rngl_one : T;
+    rngl_add : T → T → T;
+    rngl_mul : T → T → T;
+    rngl_opp : T → T;
+    rngl_inv : T → T }.
 
-Declare Scope ring_scope.
-Delimit Scope ring_scope with Rng.
+Declare Scope ring_like_scope.
+Delimit Scope ring_like_scope with F.
 
-Definition rng_sub {A} {R : ring_op A} a b := rng_add a (rng_opp b).
+Definition rngl_sub {T} {R : ring_like_op T} a b := rngl_add a (rngl_opp b).
+Definition rngl_div {T} {R : ring_like_op T} a b := rngl_mul a (rngl_inv b).
 
-Notation "0" := rng_zero : ring_scope.
-Notation "1" := rng_one : ring_scope.
-Notation "a + b" := (rng_add a b) : ring_scope.
-Notation "a - b" := (rng_sub a b) : ring_scope.
-Notation "a * b" := (rng_mul a b) : ring_scope.
-Notation "- a" := (rng_opp a) : ring_scope.
+Notation "0" := rngl_zero : ring_like_scope.
+Notation "1" := rngl_one : ring_like_scope.
+Notation "a + b" := (rngl_add a b) : ring_like_scope.
+Notation "a - b" := (rngl_sub a b) : ring_like_scope.
+Notation "a * b" := (rngl_mul a b) : ring_like_scope.
+Notation "a / b" := (rngl_div a b) : ring_like_scope.
+Notation "- a" := (rngl_opp a) : ring_like_scope.
+Notation "/ a" := (rngl_inv a) : ring_like_scope.
 
-Class ring_prop A {ro : ring_op A} :=
-  { rng_is_comm : bool;
-    rng_is_semiring : bool;
-    rng_add_comm : ∀ a b : A, (a + b = b + a)%Rng;
-    rng_add_assoc : ∀ a b c : A, (a + (b + c) = (a + b) + c)%Rng;
-    rng_add_0_l : ∀ a : A, (0 + a)%Rng = a;
-    rng_mul_assoc : ∀ a b c : A, (a * (b * c) = (a * b) * c)%Rng;
-    rng_mul_1_l : ∀ a : A, (1 * a)%Rng = a;
-    rng_mul_add_distr_l : ∀ a b c : A, (a * (b + c) = a * b + a * c)%Rng;
-    rng_mul_0_l : ∀ a, (0 * a = 0)%Rng;
-    (* when commutative *)
-    rng_c_mul_comm :
-      if rng_is_comm then ∀ a b, (a * b = b * a)%Rng else True;
-    (* when not commutative *)
-    rng_nc_mul_1_r :
-      if rng_is_comm then True else ∀ a, (a * 1 = a)%Rng;
-    rng_nc_mul_0_r :
-      if rng_is_comm then True else ∀ a, (a * 0 = 0)%Rng;
-    rng_nc_mul_add_distr_r :
-      if rng_is_comm then True else
-       ∀ a b c, ((a + b) * c = a * c + b * c)%Rng;
-    (* when ring (i.e. not a semiring) *)
-    rng_s_add_opp_l :
-      if rng_is_semiring then True else ∀ a : A, (- a + a = 0)%Rng }.
+Class ring_like_prop T {ro : ring_like_op T} :=
+  { rngl_is_comm : bool;
+    rngl_has_opp : bool;
+    rngl_has_inv : bool;
+    rngl_add_comm : ∀ a b : T, (a + b = b + a)%F;
+    rngl_add_assoc : ∀ a b c : T, (a + (b + c) = (a + b) + c)%F;
+    rngl_add_0_l : ∀ a : T, (0 + a)%F = a;
+    rngl_mul_assoc : ∀ a b c : T, (a * (b * c) = (a * b) * c)%F;
+    rngl_mul_1_l : ∀ a : T, (1 * a)%F = a;
+    rngl_mul_add_distr_l : ∀ a b c : T, (a * (b + c) = a * b + a * c)%F;
+    rngl_mul_0_l : ∀ a, (0 * a = 0)%F;
+    (* when multiplicatin is commutative *)
+    rngl_c_mul_comm :
+      if rngl_is_comm then ∀ a b, (a * b = b * a)%F else True;
+    (* when multiplicatin is not commutative *)
+    rngl_nc_mul_1_r :
+      if rngl_is_comm then True else ∀ a, (a * 1 = a)%F;
+    rngl_nc_mul_0_r :
+      if rngl_is_comm then True else ∀ a, (a * 0 = 0)%F;
+    rngl_nc_mul_add_distr_r :
+      if rngl_is_comm then True else
+       ∀ a b c, ((a + b) * c = a * c + b * c)%F;
+    (* when has opposite *)
+    rngl_o_add_opp_l :
+      if rngl_has_opp then ∀ a : T, (- a + a = 0)%F else True;
+    (* when has inverse *)
+    rngl_i_mul_inv_l :
+      if rngl_has_inv then ∀ a : T, a ≠ 0%F → (/ a * a = 1)%F else True }.
 
-(* decidability of equality in rings
+(* decidability of equality in ring_likes
    and the fact that 1 ≠ 0 *)
 
-Class ring_dec_prop T {ro : ring_op T} :=
-  { rng_eq_dec : ∀ a b : T, {a = b} + {a ≠ b};
-    rng_1_neq_0 : (1 ≠ 0)%Rng }.
+Class ring_like_dec_prop T {ro : ring_like_op T} :=
+  { rngl_eq_dec : ∀ a b : T, {a = b} + {a ≠ b};
+    rngl_1_neq_0 : (1 ≠ 0)%F }.
 
-Arguments rng_eq_dec {T}%type {ro ring_dec_prop} _%Rng _%Rng.
+Arguments rngl_eq_dec {T}%type {ro ring_like_dec_prop} _%F _%F.
 
-Fixpoint rng_power {A} {R : ring_op A} a n :=
+Fixpoint rngl_power {T} {R : ring_like_op T} a n :=
   match n with
-  | O => 1%Rng
-  | S m => (a * rng_power a m)%Rng
+  | O => 1%F
+  | S m => (a * rngl_power a m)%F
   end.
 
-Notation "a ^ b" := (rng_power a b) : ring_scope.
+Notation "a ^ b" := (rngl_power a b) : ring_like_scope.
 
-Section ring_theorems.
+Section ring_like_theorems.
 
-Context {A : Type}.
-Context {ro : ring_op A}.
-Context {rp : ring_prop A}.
+Context {T : Type}.
+Context {ro : ring_like_op T}.
+Context {rp : ring_like_prop T}.
+Context {Hro : rngl_has_opp = true}.
 
-Theorem rng_add_0_r : ∀ a, (a + 0 = a)%Rng.
+Theorem rngl_add_0_r : ∀ a, (a + 0 = a)%F.
 Proof.
 intros a; simpl.
-rewrite rng_add_comm.
-apply rng_add_0_l.
+rewrite rngl_add_comm.
+apply rngl_add_0_l.
 Qed.
 
-Theorem rng_add_add_swap : ∀ n m p, (n + m + p = n + p + m)%Rng.
+Theorem rngl_add_add_swap : ∀ n m p, (n + m + p = n + p + m)%F.
 Proof.
 intros n m p; simpl.
-do 2 rewrite <- rng_add_assoc.
-assert (m + p = p + m)%Rng as H by apply rng_add_comm.
+do 2 rewrite <- rngl_add_assoc.
+assert (m + p = p + m)%F as H by apply rngl_add_comm.
 rewrite H; reflexivity.
 Qed.
 
-Theorem rng_mul_add_distr_r : ∀ x y z,
-  ((x + y) * z = x * z + y * z)%Rng.
+Theorem rngl_mul_add_distr_r : ∀ x y z,
+  ((x + y) * z = x * z + y * z)%F.
 Proof.
 intros x y z; simpl.
-specialize rng_c_mul_comm as rng_mul_comm.
-specialize rng_nc_mul_add_distr_r as rng_mul_add_distr_r.
-destruct rng_is_comm. {
-  rewrite rng_mul_comm.
-  rewrite rng_mul_add_distr_l.
-  rewrite rng_mul_comm.
-  now rewrite (rng_mul_comm z).
+specialize rngl_c_mul_comm as rngl_mul_comm.
+specialize rngl_nc_mul_add_distr_r as rngl_mul_add_distr_r.
+destruct rngl_is_comm. {
+  rewrite rngl_mul_comm.
+  rewrite rngl_mul_add_distr_l.
+  rewrite rngl_mul_comm.
+  now rewrite (rngl_mul_comm z).
 } {
-  apply rng_mul_add_distr_r.
+  apply rngl_mul_add_distr_r.
 }
 Qed.
 
-Theorem rng_add_compat_l : ∀ a b c,
-  (a = b)%Rng → (c + a = c + b)%Rng.
+Theorem rngl_add_compat_l : ∀ a b c,
+  (a = b)%F → (c + a = c + b)%F.
 Proof.
 intros a b c Hab.
 now rewrite Hab.
 Qed.
 
-Theorem rng_add_compat_r : ∀ a b c,
-  (a = b)%Rng → (a + c = b + c)%Rng.
+Theorem rngl_add_compat_r : ∀ a b c,
+  (a = b)%F → (a + c = b + c)%F.
 Proof.
 intros a b c Hab.
 now rewrite Hab.
 Qed.
 
-Theorem rng_mul_1_r : ∀ a, (a * 1 = a)%Rng.
+Theorem rngl_mul_1_r : ∀ a, (a * 1 = a)%F.
 Proof.
 intros.
-specialize rng_c_mul_comm as rng_mul_comm.
-specialize rng_nc_mul_1_r as rng_mul_1_r.
-destruct rng_is_comm. {
-  now rewrite rng_mul_comm, rng_mul_1_l.
+specialize rngl_c_mul_comm as rngl_mul_comm.
+specialize rngl_nc_mul_1_r as rngl_mul_1_r.
+destruct rngl_is_comm. {
+  now rewrite rngl_mul_comm, rngl_mul_1_l.
 } {
-  apply rng_mul_1_r.
+  apply rngl_mul_1_r.
 }
 Qed.
 
-Theorem rng_mul_0_r : ∀ a, (a * 0 = 0)%Rng.
+Theorem rngl_mul_0_r : ∀ a, (a * 0 = 0)%F.
 Proof.
 intros.
-specialize rng_c_mul_comm as rng_mul_comm.
-specialize rng_nc_mul_0_r as rng_mul_0_r.
-destruct rng_is_comm. {
-  rewrite rng_mul_comm.
-  apply rng_mul_0_l.
+specialize rngl_c_mul_comm as rngl_mul_comm.
+specialize rngl_nc_mul_0_r as rngl_mul_0_r.
+destruct rngl_is_comm. {
+  rewrite rngl_mul_comm.
+  apply rngl_mul_0_l.
 } {
-  apply rng_mul_0_r.
+  apply rngl_mul_0_r.
 }
 Qed.
 
-Theorem fold_rng_sub : ∀ a b, (a + - b)%Rng = (a - b)%Rng.
+Theorem fold_rngl_sub : ∀ a b, (a + - b)%F = (a - b)%F.
 Proof. intros; easy. Qed.
 
-Theorem rng_add_opp_r :
-  if rng_is_semiring then True else ∀ x, (x - x = 0)%Rng.
-Proof.
-specialize rng_s_add_opp_l as rng_add_opp_l.
-destruct rng_is_semiring; [ easy | ].
-intros.
-unfold rng_sub.
-now rewrite rng_add_comm.
-Qed.
-
-... mouais... chais pas... je vais me retrouver avec des
-    tas de théorèmes, avec des soustractions, conditionnés
-    par rng_is_semiring... j'aime pas trop ça...
-
-Theorem rng_add_sub : ∀ a b, (a + b - b = a)%Rng.
+Theorem rngl_add_opp_l : ∀ x, (- x + x = 0)%F.
 Proof.
 intros.
-unfold rng_sub.
-rewrite <- rng_add_assoc.
-rewrite fold_rng_sub.
-rewrite rng_add_opp_r.
-apply rng_add_0_r.
+specialize rngl_o_add_opp_l as rngl_add_opp_l.
+destruct rngl_has_opp; [ | easy ].
+apply rngl_add_opp_l.
 Qed.
 
-Theorem rng_add_reg_r : ∀ a b c, (a + c = b + c)%Rng → (a = b)%Rng.
+Theorem rngl_add_opp_r : ∀ x, (x - x = 0)%F.
+Proof.
+intros.
+specialize rngl_o_add_opp_l as rngl_add_opp_l.
+destruct rngl_has_opp; [ | easy ].
+unfold rngl_sub.
+rewrite rngl_add_comm.
+apply rngl_add_opp_l.
+Qed.
+
+Theorem rngl_add_sub : ∀ a b, (a + b - b = a)%F.
+Proof.
+intros.
+unfold rngl_sub.
+rewrite <- rngl_add_assoc.
+rewrite fold_rngl_sub.
+rewrite rngl_add_opp_r.
+apply rngl_add_0_r.
+Qed.
+
+(*
+End ring_like_theorems.
+
+Check @rngl_add_opp_l.
+Check @rngl_add_opp_r.
+Check @rngl_o_add_opp_l.
+Check @rngl_add_sub.
+Arguments rngl_add_opp_r {T ro rp Hro} x%F.
+*)
+
+...
+
+Theorem rngl_add_reg_r : ∀ a b c, (a + c = b + c)%F → (a = b)%F.
 Proof.
 intros a b c Habc; simpl in Habc; simpl.
-eapply rng_sub_compat_l with (c := c) in Habc.
-now do 2 rewrite rng_add_sub in Habc.
+eapply rngl_sub_compat_l with (c := c) in Habc.
+now do 2 rewrite rngl_add_sub in Habc.
 Qed.
 
-Theorem rng_add_reg_l : ∀ a b c, (c + a = c + b)%Rng → (a = b)%Rng.
+Theorem rngl_add_reg_l : ∀ a b c, (c + a = c + b)%F → (a = b)%F.
 Proof.
 intros a b c Habc; simpl in Habc; simpl.
-apply rng_add_reg_r with (c := c).
-rewrite rng_add_comm; symmetry.
-now rewrite rng_add_comm; symmetry.
+apply rngl_add_reg_r with (c := c).
+rewrite rngl_add_comm; symmetry.
+now rewrite rngl_add_comm; symmetry.
 Qed.
 
-Theorem rng_opp_0 : (- 0 = 0)%Rng.
+Theorem rngl_opp_0 : (- 0 = 0)%F.
 Proof.
-transitivity (0 + - 0)%Rng. {
+transitivity (0 + - 0)%F. {
   symmetry.
-  apply rng_add_0_l.
+  apply rngl_add_0_l.
 }
-apply rng_add_opp_r.
+apply rngl_add_opp_r.
 Qed.
 
-Theorem rng_sub_0_r : ∀ a, (a - 0 = a)%Rng.
+Theorem rngl_sub_0_r : ∀ a, (a - 0 = a)%F.
 Proof.
 intros.
-unfold rng_sub.
-rewrite rng_opp_0.
-apply rng_add_0_r.
+unfold rngl_sub.
+rewrite rngl_opp_0.
+apply rngl_add_0_r.
 Qed.
 
-Theorem rng_add_move_0_r : ∀ a b, (a + b = 0)%Rng ↔ (a = - b)%Rng.
+Theorem rngl_add_move_0_r : ∀ a b, (a + b = 0)%F ↔ (a = - b)%F.
 Proof.
 intros a b.
 split; intros H. {
-  apply rng_sub_compat_l with (c := b) in H.
-  rewrite rng_add_sub in H.
-  unfold rng_sub in H.
-  now rewrite rng_add_0_l in H.
+  apply rngl_sub_compat_l with (c := b) in H.
+  rewrite rngl_add_sub in H.
+  unfold rngl_sub in H.
+  now rewrite rngl_add_0_l in H.
 } {
   rewrite H.
-  now rewrite rng_add_opp_l.
+  now rewrite rngl_add_opp_l.
 }
 Qed.
 
-Theorem rng_opp_involutive : ∀ x, (- - x)%Rng = x.
+Theorem rngl_opp_involutive : ∀ x, (- - x)%F = x.
 Proof.
 intros.
 symmetry.
-apply rng_add_move_0_r.
-apply rng_add_opp_r.
+apply rngl_add_move_0_r.
+apply rngl_add_opp_r.
 Qed.
 
-Theorem rng_mul_opp_l : ∀ a b, (- a * b = - (a * b))%Rng.
+Theorem rngl_mul_opp_l : ∀ a b, (- a * b = - (a * b))%F.
 Proof.
 intros.
-specialize (rng_mul_add_distr_r (- a)%Rng a b) as H.
-rewrite rng_add_opp_l in H.
-rewrite rng_mul_0_l in H.
+specialize (rngl_mul_add_distr_r (- a)%F a b) as H.
+rewrite rngl_add_opp_l in H.
+rewrite rngl_mul_0_l in H.
 symmetry in H.
-now apply rng_add_move_0_r in H.
+now apply rngl_add_move_0_r in H.
 Qed.
 
-Theorem rng_mul_opp_r : ∀ a b, (a * - b = - (a * b))%Rng.
+Theorem rngl_mul_opp_r : ∀ a b, (a * - b = - (a * b))%F.
 Proof.
 intros.
-specialize (rng_mul_add_distr_l a b (- b)%Rng) as H.
-rewrite fold_rng_sub in H.
-rewrite rng_add_opp_r in H.
-rewrite rng_mul_0_r in H.
+specialize (rngl_mul_add_distr_l a b (- b)%F) as H.
+rewrite fold_rngl_sub in H.
+rewrite rngl_add_opp_r in H.
+rewrite rngl_mul_0_r in H.
 symmetry in H.
-rewrite rng_add_comm in H.
-now apply rng_add_move_0_r in H.
+rewrite rngl_add_comm in H.
+now apply rngl_add_move_0_r in H.
 Qed.
 
-Theorem rng_mul_opp_opp : ∀ a b, (- a * - b = a * b)%Rng.
+Theorem rngl_mul_opp_opp : ∀ a b, (- a * - b = a * b)%F.
 Proof.
 intros.
-rewrite rng_mul_opp_l.
-rewrite rng_mul_opp_r.
-apply rng_opp_involutive.
+rewrite rngl_mul_opp_l.
+rewrite rngl_mul_opp_r.
+apply rngl_opp_involutive.
 Qed.
 
-Theorem rng_opp_inj : ∀ a b, (- a = - b)%Rng → a = b.
+Theorem rngl_opp_inj : ∀ a b, (- a = - b)%F → a = b.
 Proof.
 intros.
-rewrite <- (rng_opp_involutive a).
+rewrite <- (rngl_opp_involutive a).
 rewrite H.
-apply rng_opp_involutive.
+apply rngl_opp_involutive.
 Qed.
 
-End ring_theorems.
+End ring_like_theorems.
