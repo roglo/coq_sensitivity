62e9ebe6731a22aa65307819b9e843a20e5e106a
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-12-06 23:34:52 +0100

    -

--- Zring.v
+++ Zring.v
@@ -4,37 +4,38 @@ Require Import Utf8 ZArith.
 
 Require Import Semiring.
 
-Definition Z_semiring_op : semiring_op Z :=
-  {| srng_zero := 0%Z;
-     srng_one := 1%Z;
-     srng_add := Z.add;
-     srng_mul := Z.mul |}.
+Definition Z_inv (x : Z) := 0%Z.
 
-Definition Z_ring_op : ring_op Z :=
-  {| rng_opp := Z.opp |}.
+Definition Z_ring_like_op : ring_like_op Z :=
+  {| rngl_zero := 0%Z;
+     rngl_one := 1%Z;
+     rngl_add := Z.add;
+     rngl_mul := Z.mul;
+     rngl_opp := Z.opp;
+     rngl_inv := Z_inv |}.
 
-Canonical Structure Z_semiring_op.
-Canonical Structure Z_ring_op.
+Canonical Structure Z_ring_like_op.
 
-Definition Z_semiring_prop : semiring_prop Z :=
-  {| srng_is_comm := true;
-     srng_add_comm := Z.add_comm;
-     srng_add_assoc := Z.add_assoc;
-     srng_add_0_l := Z.add_0_l;
-     srng_mul_assoc := Z.mul_assoc;
-     srng_mul_1_l := Z.mul_1_l;
-     srng_mul_add_distr_l := Z.mul_add_distr_l;
-     srng_mul_0_l := Z.mul_0_l;
-     srng_c_mul_comm := Z.mul_comm;
-     srng_nc_mul_1_r := I;
-     srng_nc_mul_0_r := I;
-     srng_nc_mul_add_distr_r := I |}.
-
-Definition Z_ring_prop : ring_prop Z :=
-  {| rng_add_opp_l := Z.add_opp_diag_l |}.
+Definition Z_ring_like_prop : ring_like_prop Z :=
+  {| rngl_is_comm := true;
+     rngl_has_opp := true;
+     rngl_has_inv := false;
+     rngl_add_comm := Z.add_comm;
+     rngl_add_assoc := Z.add_assoc;
+     rngl_add_0_l := Z.add_0_l;
+     rngl_mul_assoc := Z.mul_assoc;
+     rngl_mul_1_l := Z.mul_1_l;
+     rngl_mul_add_distr_l := Z.mul_add_distr_l;
+     rngl_mul_0_l := Z.mul_0_l;
+     rngl_c_mul_comm := Z.mul_comm;
+     rngl_nc_mul_1_r := I;
+     rngl_nc_mul_0_r := I;
+     rngl_nc_mul_add_distr_r := I;
+     rngl_o_add_opp_l := Z.add_opp_diag_l;
+     rngl_i_mul_inv_l := I |}.
 
 Theorem Z_1_neq_0 : 1%Z ≠ 0%Z.
 Proof. easy. Qed.
 
-Definition Z_sring_dec_prop : sring_dec_prop Z :=
-  {| srng_eq_dec := Z.eq_dec; srng_1_neq_0 := Z_1_neq_0 |}.
+Definition Z_ring_like_dec_prop : ring_like_dec_prop Z :=
+  {| rngl_eq_dec := Z.eq_dec; rngl_1_neq_0 := Z_1_neq_0 |}.
