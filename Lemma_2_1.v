2f3fdd22476620a22f8dcaadb2c75322e217d06d
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-12-08 17:23:36 +0100

    -

--- Lemma_2_1.v
+++ Lemma_2_1.v
@@ -1,9 +1,12 @@
 (* Lemma 2.1 of the proof of the sensitivity conjecture *)
 
-(* Lemma 2.1.(Cauchy’s Interlace Theorem) Let A be a symmetric nxn
-   matrix, and B be a mxm principal submatrix of A, for some m < n.
+(* Given a n×n matrix A, a principal submatrix of A is obtained by
+   deleting the same set of rows and columns from A. *)
+
+(* Lemma 2.1. (Cauchy’s Interlace Theorem) Let A be a symmetric n×n
+   matrix, and B be a m×m principal submatrix of A, for some m < n.
    If the eigenvalues of A are λ1 ≥ λ2 ≥ ... ≥ λn, and the eigenvalues
-   of B are μ1 ≥ μ2 ≥ ... ≥ μm, then for all 1 ≤ i ≤m,
+   of B are μ1 ≥ μ2 ≥ ... ≥ μm, then for all 1 ≤ i ≤ m,
      λi ≥ μi ≥ λ_{i-n+m}
  *)
 
