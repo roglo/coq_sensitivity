41bebb3782348da56e27b0ceb2f0f087ea28cc51
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-12-12 00:48:42 +0100

    -

--- Zring.v
+++ Zring.v
@@ -14,11 +14,32 @@ Canonical Structure Z_ring_like_op : ring_like_op Z :=
      rngl_opp := Z.opp;
      rngl_inv := phony_Z_inv |}.
 
+Existing Instance Z_ring_like_op.
+
 Theorem Z_eq_mul_0 :  ∀ n m, (n * m)%Z = 0%Z → n = 0%Z ∨ m = 0%Z.
 Proof. now apply Z.eq_mul_0. Qed.
 
 Theorem Z_1_neq_0 : (1 ≠ 0)%Z.
 Proof. easy. Qed.
+
+Theorem Z_characteristic_prop : ∀ i, rngl_of_nat (S i) ≠ 0%Z.
+Proof.
+intros.
+cbn - [ Z.add ].
+assert (Hz : ∀ i, (0 <= rngl_of_nat i)%Z). {
+  clear i; intros.
+  cbn - [ Z.add ].
+  induction i; [ easy | ].
+  cbn - [ Z.add ].
+  now destruct (rngl_of_nat i).
+}
+intros H.
+specialize (Hz i).
+apply Z.nlt_ge in Hz; apply Hz.
+rewrite <- H.
+apply Z.lt_sub_lt_add_r.
+now rewrite Z.sub_diag.
+Qed.
 
 Definition Z_ring_like_prop : ring_like_prop Z :=
   {| rngl_is_comm := true;
@@ -26,6 +47,7 @@ Definition Z_ring_like_prop : ring_like_prop Z :=
      rngl_has_inv := false;
      rngl_has_dec_eq := true;
      rngl_is_domain := true;
+     rngl_characteristic := 0;
      rngl_add_comm := Z.add_comm;
      rngl_add_assoc := Z.add_assoc;
      rngl_add_0_l := Z.add_0_l;
@@ -42,4 +64,5 @@ Definition Z_ring_like_prop : ring_like_prop Z :=
      rngl_opt_mul_inv_l := I;
      rngl_opt_mul_inv_r := I;
      rngl_opt_eq_dec := Z.eq_dec;
-     rngl_opt_is_integral := Z_eq_mul_0 |}.
+     rngl_opt_is_integral := Z_eq_mul_0;
+     rngl_characteristic_prop := Z_characteristic_prop |}.
