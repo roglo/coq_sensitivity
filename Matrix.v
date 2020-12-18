267290224c1e48ef333cbc3fc90ea08d895256cf
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-12-18 13:38:18 +0100

    -

--- Matrix.v
+++ Matrix.v
@@ -100,14 +100,14 @@ Proof. easy. Qed.
 
 (* opposite *)
 
-Definition mat_opp {ro : ring_like_op T} {m n} (M : matrix m n T) :
-  matrix m n T :=
-  {| mat_el i j := (- mat_el M i j)%F |}.
+Definition mat_opt_opp {m n} M : matrix m n T :=
+  if rngl_has_opp then {| mat_el i j := (- mat_el M i j)%F |}
+  else M.
 
 (* subtraction *)
 
-Definition mat_sub {m n} (MA MB : matrix m n T) :=
-  mat_add MA (mat_opp MB).
+Definition mat_sub {m n} (MA MB : matrix m n T) : matrix m n T :=
+  {| mat_el i j := (mat_el MA i j - mat_el MB i j)%F |}.
 
 (* *)
 
@@ -354,7 +354,9 @@ Notation "A + B" := (mat_add A B) : M_scope.
 Notation "A - B" := (mat_sub A B) : M_scope.
 Notation "A * B" := (mat_mul A B) : M_scope.
 Notation "μ × A" := (mat_mul_scal_l μ A) (at level 40) : M_scope.
+(*
 Notation "- A" := (mat_opp A) : M_scope.
+*)
 
 Declare Scope V_scope.
 Delimit Scope V_scope with V.
@@ -371,9 +373,11 @@ Notation "U · V" := (vect_dot_product U V) (at level 40) : V_scope.
 
 (* *)
 
+(*
 Theorem mat_fold_sub : ∀ {m n} (MA MB : matrix m n T),
   (MA + - MB = MA - MB)%M.
 Proof. easy. Qed.
+*)
 
 (* commutativity of addition *)
 
@@ -417,28 +421,30 @@ Qed.
 
 (* addition left and right with opposite *)
 
+Arguments mat_opt_opp {T}%type {ro} {m n}%nat M%M.
+
 Theorem mat_add_opp_l {m n} :
   rngl_has_opp = true →
-  ∀ (M : matrix m n T), (- M + M = mZ m n)%M.
+  ∀ (M : matrix m n T), (mat_opt_opp M + M = mZ m n)%M.
 Proof.
 intros Hro *.
 apply matrix_eq.
 intros * Hi Hj.
+unfold mat_opt_opp.
+rewrite Hro; cbn.
 now apply rngl_add_opp_l.
 Qed.
 
 Theorem mat_add_opp_r :
-  rngl_has_opp = true →
   ∀ {m n} (M : matrix m n T),
   (M - M = mZ m n)%M.
 Proof.
-intros Hro *.
+intros.
 apply matrix_eq.
 intros * Hi Hj.
 specialize rngl_add_opp_r as H.
-cbn; unfold rngl_sub in H.
-rewrite Hro in H.
-apply H.
+cbn; unfold rngl_sub in H |-*.
+destruct rngl_has_opp; apply H.
 Qed.
 
 (*
@@ -1019,7 +1025,7 @@ Canonical Structure mat_ring_like_op n :
      rngl_one := mI n;
      rngl_add := @mat_add T _ n n;
      rngl_mul := @mat_mul T _ n n n;
-     rngl_opp := @mat_opp T _ n n;
+     rngl_opp := @mat_opt_opp T _ n n;
      rngl_inv := @phony_squ_mat_inv n;
      rngl_le := @phony_squ_mat_le n;
      rngl_opt_sub := @phony_squ_mat_sub n;
@@ -1462,28 +1468,40 @@ destruct x; [ | easy ].
 now apply H.
 Qed.
 
-Theorem mat_opt_add_sub : ∀ n (ro := mat_ring_like_op n),
-  if rngl_has_opp then True else ∀ a b : matrix n n T, (a + b - b)%M = a.
+Theorem mat_opt_add_sub : ∀ n,
+  match @rngl_has_opp (matrix n n T) (mat_ring_like_op n) return Prop with
+  | true => True
+  | false =>
+      forall a b : matrix n n T,
+      @eq (matrix n n T)
+        (@rngl_sub (matrix n n T) (mat_ring_like_op n) (@rngl_add (matrix n n T) (mat_ring_like_op n) a b) b) a
+  end.
+intros.
+remember rngl_has_opp as x eqn:Hx; symmetry in Hx.
+destruct x; [ easy | ].
+intros MA MB.
+apply matrix_eq; cbn.
+intros * Hi Hj.
+unfold rngl_sub.
+rewrite Hx.
+cbn.
+...
+apply rngl_add_sub.
+
+Theorem mat_opt_add_sub : ∀ n,
+  if @rngl_has_opp (matrix n n T) (mat_ring_like_op n) then True
+  else ∀ a b : matrix n n T, (a + b - b)%M = a.
 Proof.
 intros.
 remember rngl_has_opp as x eqn:Hx; symmetry in Hx.
 destruct x; [ easy | ].
 intros MA MB.
-Theorem mat_add_sub : ∀ m n (MA MB : matrix m n T), (MA + MB - MB)%M = MA.
-Admitted.
-...
-apply mat_add_sub.
-...
-
-intros MA MB.
-apply matrix_eq.
+apply matrix_eq; cbn.
 intros * Hi Hj.
-cbn.
-Search ((_ + _) - _)%M.
-...
-now apply H.
+apply rngl_add_sub.
 Qed.
-...
+
+Set Printing All.
 
 Definition squ_mat_ring_like_prop (n : nat) :
   ring_like_prop (matrix n n T) :=
