3d27243a98db54819570bbf996c9e878e23d0d23
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-01-10 09:58:59 +0100

    -

--- Determinant.v
+++ Determinant.v
@@ -1411,6 +1411,11 @@ cbn - [ iter_seq ].
 replace (Π (i = _, _), Π (j = _, _), _)%F with
   (Π (k = 1, n), Π (l = 1, n),
    (sgn_diff (vect_el σ₁ k) (vect_el σ₁ l) * sgn_diff k l))%F. 2: {
+(* finalement, c'est aussi compliqué d'essayer de prouver ça ; je me
+   retrouve systématiquement soit avec une somme de produits censés
+   s'annuler 2 à 2, mais faut formaliser ça, soit, ici, avec un
+   produit de produits censé être égal à un autre produit de produits,
+   dans un autre ordre, et, pour le formaliser, c'est pas mieux *)
 ...
 erewrite rngl_product_eq_compat. 2: {
   intros i Hi.
