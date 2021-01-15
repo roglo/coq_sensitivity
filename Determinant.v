382d089b162cb880e4b1eb5c2602b438b6e6a33f
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-01-15 11:51:05 +0100

    -

--- Determinant.v
+++ Determinant.v
@@ -1606,6 +1606,20 @@ Theorem permut_has_invert : ∀ n (σ : vector n nat),
 Proof.
 intros * Hperm.
 destruct Hperm as (Hp1, Hp2).
+Search canon_permut.
+Print canon_permut_inv.
+Print find.
+Fixpoint permut_find n (σ : vector n nat) i j :=
+  match i with
+  | 0 => 0
+  | S i' => if Nat.eq_dec (vect_el σ i) j then i else permut_find σ i' j
+  end.
+Definition permut_inv n (σ : vector n nat) :=
+  mk_vect n (permut_find σ n).
+Compute list_of_vect (canon_permut 4 7).
+Compute list_of_vect (permut_inv (canon_permut 4 7)).
+...
+Check permut_inv.
 ...
 
 Theorem signature_comp :
