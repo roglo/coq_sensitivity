54a91e838718a2ef207cf7f8f64e2c03e179aac4
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-02-04 11:54:56 +0100

    -

--- MyVector.v
+++ MyVector.v
@@ -105,6 +105,102 @@ rewrite rngl_mul_opp_r; [ | easy ].
 easy.
 Qed.
 
+Declare Scope V_scope.
+Delimit Scope V_scope with V.
+
+Notation "μ × V" := (vect_mul_scal_l μ V) (at level 40) : V_scope.
+Notation "≺ U , V ≻" := (vect_dot_product U V) (at level 35).
+Notation "μ × V" := (vect_mul_scal_l μ V) (at level 40) : V_scope.
+
+Theorem vect_mul_scal_l_mul_assoc {n} : ∀ (a b : T) (V : vector n T),
+  (a × (b × V))%V = ((a * b)%F × V)%V.
+Proof.
+intros.
+apply vector_eq.
+intros * Hi; cbn.
+apply rngl_mul_assoc.
+Qed.
+
+Theorem vect_mul_scal_reg_r :
+  rngl_has_dec_eq = true →
+  rngl_has_inv = true ∨ rngl_has_no_inv_but_div = true →
+  ∀ {n} (V : vector n T) a b,
+  V ≠ vect_zero n
+  → (a × V = b × V)%V
+  → a = b.
+Proof.
+intros Hde Hii * Hvz Hab.
+assert (Hiv : ∀ i, vect_el (a × V)%V i = vect_el (b × V)%V i). {
+  intros i.
+  now rewrite Hab.
+}
+unfold vect_mul_scal_l in Hiv.
+cbn in Hiv.
+assert (Hn : ¬ ∀ i, i < n → vect_el V i = 0%F). {
+  intros H; apply Hvz.
+  apply vector_eq.
+  cbn; intros * Hi.
+  now apply H.
+}
+assert (∃ i, vect_el V i ≠ 0%F). {
+  specialize rngl_opt_eq_dec as rngl_eq_dec.
+  rewrite Hde in rngl_eq_dec.
+  apply (not_forall_in_interv_imp_exist (a:=0) (b:=n-1));
+    cycle 1. {
+    flia.
+  } {
+    intros Hnv.
+    apply Hn.
+    intros i Hi.
+    specialize (Hnv i).
+    assert (H : 0 ≤ i ≤ n - 1) by flia Hi.
+    specialize (Hnv H).
+    now destruct (rngl_eq_dec (vect_el V i) 0%F).
+  }
+  intros k.
+  unfold Decidable.decidable.
+  specialize (rngl_eq_dec (vect_el V k) 0%F) as [Hvnz| Hvnz]. {
+    now right.
+  } {
+    now left.
+  }
+}
+move Hiv at bottom.
+destruct H as (i, Hi).
+specialize (Hiv i).
+now apply rngl_mul_reg_r in Hiv.
+Qed.
+
+Arguments vect_dot_product {n}%nat (U V)%V.
+
+...
+
+Theorem vect_dot_mul_scal_mul_comm :
+  rngl_is_comm = true →
+  ∀ {n} (a : T) (U V : vector n T),
+  ≺ U, a × V ≻ = (a * ≺ U, V ≻)%F.
+Proof.
+intros Hic *.
+unfold vect_dot_product.
+rewrite rngl_mul_summation_distr_l.
+apply rngl_summation_eq_compat.
+intros j Hj; cbn.
+do 2 rewrite rngl_mul_assoc.
+f_equal.
+now apply rngl_mul_comm.
+Qed.
+
+Theorem vect_scal_mul_dot_mul_comm : ∀ {n} (a : T) (U V : vector n T),
+  ≺ a × U, V ≻ = (a * ≺ U, V ≻)%F.
+Proof.
+intros.
+unfold vect_dot_product.
+rewrite rngl_mul_summation_distr_l.
+apply rngl_summation_eq_compat.
+intros j Hj; cbn.
+symmetry; apply rngl_mul_assoc.
+Qed.
+
 End a.
 
 Declare Scope V_scope.
