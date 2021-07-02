8e9a26e88ab47604883fcbf9ef8c023bd00d0487
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-07-02 09:43:57 +0200

    -

--- MyVector.v
+++ MyVector.v
@@ -342,9 +342,8 @@ destruct IHn as [IHn| IHn]. {
     enough (Fin.FS (Fin.of_nat_lt H4) = i) by congruence.
     clear - H4.
     induction n; [ easy | ].
-Definition glop n (i : Fin.t (S n)) (p : proj1_sig (Fin.to_nat i) < n) : Fin.t n.
-clear - p.
-Admitted.
+Definition glop n (i : Fin.t (S n)) (p : proj1_sig (Fin.to_nat i) < n) :=
+  Fin.of_nat_lt p.
 specialize (@glop (S n) i H4) as j.
 ,,,
 Search Fin.of_nat_lt.
