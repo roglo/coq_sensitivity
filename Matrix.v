1086a0baa7bb6f42002b03df44db6e6230010a5f
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-07-12 17:53:22 +0200

    -

--- Matrix.v
+++ Matrix.v
@@ -321,6 +321,11 @@ Proof.
 intros.
 unfold mZ, "+"%M, mat_nrows, mat_ncols.
 destruct M as (ll); cbn; f_equal.
+induction ll as [| l]; [ easy | cbn ].
+(* ouais, en fait c'est faux; il faut que M soit une matrice "bien structurée",
+   c'est-à-dire dont toutes les lignes aient la même longueur *)
+(* ou alors définir un mZ qui ait la même structure que M; je crois que j'avais
+   fait ça, avant. Mais chais pas *)
 ...
 intros.
 apply matrix_eq.
