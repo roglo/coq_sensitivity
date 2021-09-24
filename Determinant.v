480b9dae1d7a88869e0e01d17824f94223177751
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-09-24 13:23:08 +0200

    -

--- Determinant.v
+++ Determinant.v
@@ -2605,7 +2605,7 @@ Definition comatrix n (M : matrix T) : matrix T :=
     (map
       (λ i,
        map
-         (λ j, (minus_one_pow (i + j) * determinant n (subm M i j))%F)
+         (λ j, (minus_one_pow (i + j) * determinant (n - 1) (subm M i j))%F)
          (seq 0 (mat_ncols M)))
       (seq 0 (mat_nrows M))).
 
@@ -3585,70 +3585,47 @@ destruct (Nat.eq_dec i 0) as [Hiz| Hiz]. {
   rewrite rngl_mul_comm; [ | easy ].
   f_equal.
   remember (S n) as sn; cbn; subst sn.
-Inspect 1.
-...
-  specialize (square_matrix_ncols M Hsm) as Hc.
-  specialize (proj1 (is_sm_mat_iff (S n) M) Hsm) as H1.
-  destruct H1 as (Hr & Hcr & Hc').
+  rewrite Nat_sub_succ_1.
   rewrite (List_map_nth' 0); [ | rewrite seq_length; flia Hr ].
-  rewrite (List_map_nth' 0); [ | rewrite seq_length, Hc; flia Hj ].
+  rewrite (List_map_nth' 0); [ | rewrite seq_length; flia Hc Hj ].
   rewrite seq_nth; [ | flia Hr ].
   rewrite seq_nth; [ | flia Hc Hj ].
-  do 3 rewrite Nat.add_0_l.
-  f_equal.
-...
+  now do 3 rewrite Nat.add_0_l.
+}
 symmetry.
 erewrite rngl_summation_eq_compat. 2: {
-  intros j Hj; cbn.
+  intros j (_, Hj); cbn; rewrite Nat.sub_0_r.
   rewrite (List_map_nth' 0); [ | now rewrite seq_length, Hr ].
   rewrite (List_map_nth' 0); [ | rewrite seq_length, Hc; flia Hj ].
   rewrite seq_nth; [ | now rewrite Hr ].
   rewrite seq_nth; [ | rewrite Hc; flia Hj ].
   do 2 rewrite Nat.add_0_l.
+(*
   rewrite rngl_mul_comm; [ | easy ].
   rewrite rngl_mul_mul_swap; [ | easy ].
   rewrite minus_one_pow_add_r; [ | easy ].
+  rewrite rngl_mul_comm; [ | easy ].
+  rewrite rngl_mul_mul_swap; [ | easy ].
   do 2 rewrite <- rngl_mul_assoc.
-(*
   remember (minus_one_pow i * _)%F as x eqn:Hx.
   rewrite <- rngl_opp_involutive in Hx; [ | easy ].
   rewrite <- rngl_mul_opp_l in Hx; [ | easy ].
 *)
   specialize determinant_subm_mat_swap_rows_0_i as H1.
   specialize (H1 Hic Hop Hin Hit H10 Hde Hch).
-  specialize (H1 (S n) M i j Hsm).
-...
-  cbn in H1.
-  rewrite <- H1 in Hx; [ | flia Hiz Hlin ].
-  subst x; clear H1.
-  rewrite rngl_mul_comm; [ | easy ].
-  rewrite rngl_mul_assoc, rngl_mul_mul_swap; [ | easy ].
-  replace (mat_el M p j) with (mat_el (mat_swap_rows 0 p M) 0 j) by easy.
-  rewrite <- HeqM'.
-  rewrite rngl_mul_opp_r; [ | easy ].
-  easy.
-...
-  rewrite rngl_mul_comm; [ | easy ].
-  rewrite rngl_mul_assoc.
-...
-  remember (minus_one_pow p * _)%F as x eqn:Hx.
-  rewrite <- rngl_opp_involutive in Hx; [ | easy ].
-  rewrite <- rngl_mul_opp_l in Hx; [ | easy ].
-  specialize determinant_subm_mat_swap_rows_0_i as H1.
-  specialize (H1 Hic Hop Hin Hit H10 Hde Hch).
-  specialize (H1 _ M p j).
-  cbn in H1.
-  rewrite <- H1 in Hx; [ | flia Hiz Hlin ].
-  subst x; clear H1.
-  rewrite rngl_mul_comm; [ | easy ].
-  rewrite rngl_mul_assoc, rngl_mul_mul_swap; [ | easy ].
-  replace (mat_el M p j) with (mat_el (mat_swap_rows 0 p M) 0 j) by easy.
-  rewrite <- HeqM'.
-  rewrite rngl_mul_opp_r; [ | easy ].
+  specialize (H1 n M i j Hsm).
+  assert (H : 0 < i ≤ n) by flia Hiz Hlin.
+  specialize (H1 H Hj); clear H.
+  rewrite rngl_mul_opp_l in H1; [ | easy ].
+  symmetry in H1.
+  rewrite <- rngl_opp_involutive in H1; [ | easy ].
+  apply rngl_opp_inj in H1; [ | easy ].
+  rewrite minus_one_pow_add_r; [ | easy ].
+  rewrite rngl_mul_mul_swap; [ | easy ].
+  rewrite H1.
   easy.
 }
 cbn.
-...
 ...
 intros Hic Hop Hin Hit H10 Hde Hch * Hsm Hlin.
 destruct (Nat.eq_dec i 0) as [Hiz| Hiz]. {
