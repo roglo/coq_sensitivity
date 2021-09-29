a599e7887c65c75a3cb84edef74c1d257442b358
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-09-29 13:39:56 +0200

    -

--- Determinant.v
+++ Determinant.v
@@ -3746,16 +3746,25 @@ assert (H : ∀ i : nat, i < S n → vect_nat_el σ' i < S n). {
   flia H1 H4.
 }
 specialize (IHn H); clear H.
-...
-assert (H : ∀ i j, i < S n → j < S n → vect_el σ' i = vect_el σ' j → i = j). {
-  intros i j Hi Hj Hij; cbn in Hij.
-  unfold g in Hij; cbn in Hij.
-  destruct (Nat.eq_dec (vect_el σ (S n)) (S n)) as [H3| H3]. {
+assert
+  (H : ∀ i j, i < S n → j < S n →
+   vect_nat_el σ' i = vect_nat_el σ' j → i = j). {
+  intros i j Hi Hj Hij; cbn - [ seq ] in Hij.
+  rewrite (List_map_nth' 0) in Hij; [ | now rewrite seq_length ].
+  rewrite (List_map_nth' 0) in Hij; [ | now rewrite seq_length ].
+  rewrite seq_nth in Hij; [ | easy ].
+  rewrite seq_nth in Hij; [ | easy ].
+  do 2 rewrite Nat.add_0_l in Hij.
+  unfold g in Hij; cbn - [ seq ] in Hij.
+  rewrite (List_map_nth' 0) in Hij; [ | rewrite seq_length; flia ].
+  rewrite seq_nth in Hij; [ | flia ].
+  rewrite Nat.add_0_l in Hij.
+  destruct (Nat.eq_dec (vect_nat_el σ (S n)) (S n)) as [H3| H3]. {
     destruct (lt_dec i (S n)) as [H| H]; [ clear H | flia Hi H ].
     destruct (lt_dec j (S n)) as [H| H]; [ clear H | flia Hj H ].
     apply H2; [ flia Hi | flia Hj | easy ].
   }
-  destruct (Nat.eq_dec (vect_el σ n) (S n)) as [H4| H4]. {
+  destruct (Nat.eq_dec (vect_nat_el σ n) (S n)) as [H4| H4]. {
     destruct (lt_dec i n) as [Hin| Hin]. {
       destruct (lt_dec j n) as [Hjn| Hjn]. {
         apply H2; [ flia Hi | flia Hj | easy ].
@@ -3772,7 +3781,7 @@ assert (H : ∀ i j, i < S n → j < S n → vect_el σ' i = vect_el σ' j → i
     }
     now replace j with n by flia Hj Hjn.
   }
-  remember (permut_fun_inv (vect_el σ) n (S n)) as k eqn:Hk.
+  remember (permut_fun_inv (vect_nat_el σ) n (S n)) as k eqn:Hk.
   destruct (lt_dec i k) as [H5| H5]. {
     destruct (lt_dec j k) as [H6| H6]. {
       apply H2; [ flia Hi | flia Hj | easy ].
@@ -3788,25 +3797,114 @@ assert (H : ∀ i j, i < S n → j < S n → vect_el σ' i = vect_el σ' j → i
   flia Hij.
 }
 specialize (IHn H); clear H.
-remember (vect_el (permut_inv σ) (S n)) as k eqn:Hk.
+remember (vect_nat_el (permut_inv (S (S n)) σ) (S n)) as k eqn:Hk.
 destruct (Nat.eq_dec k (S n)) as [Hksn| Hksn]. {
   erewrite rngl_product_eq_compat in IHn. 2: {
     intros i Hi.
-    unfold σ', g; cbn.
+    unfold σ', g; cbn - [ seq ].
+    rewrite (List_map_nth' 0); [ | rewrite seq_length; flia Hi ].
+    rewrite seq_nth; [ | flia Hi ].
+    rewrite Nat.add_0_l.
     destruct (lt_dec i k) as [H| H]; [ easy | flia Hksn Hi H ].
   }
   cbn in IHn.
   rewrite rngl_product_split_last; [ | flia ].
-  rewrite rngl_product_succ_succ' with (g0 := λ i, f (vect_el σ i)).
+  rewrite rngl_product_succ_succ' with (g0 := λ i, f (vect_nat_el σ i)).
   symmetry.
   rewrite rngl_product_split_last; [ | flia ].
   rewrite rngl_product_succ_succ'.
   symmetry.
   rewrite IHn; f_equal; f_equal.
+(**)
+subst k.
+unfold permut_inv in Hksn.
+cbn - [ permut_fun_inv seq ] in Hksn.
+rewrite (List_map_nth' 0) in Hksn; [ | rewrite seq_length; flia ].
+rewrite seq_nth in Hksn; [ | flia ].
+rewrite Nat.add_0_l in Hksn.
+...
+Search permut_fun_inv.
+(* voir permut_fun_inv_fun et fun_find_prop : ils font pas double
+   emploi, ceux-là ? *)
+(* et permut_fun_inv_fun' aussi !!! *)
+Check fun_permut_fun_inv.
+Check fun_find_prop.
+generalize Hksn; intros H3.
+cbn in Hksn.
+  destruct (Nat.eq_dec (vect_nat_el σ (S n)) (S n)) as [Hσn| Hσn]; [ easy | ].
+  destruct (Nat.eq_dec (vect_nat_el σ n) (S n)) as [Hσs| Hσs]. {
+    flia Hksn.
+  }
+Search permut_fun_inv.
+...
+symmetry in H3.
+apply permut_fun_without_last in H3; [ | easy ].
+destruct H3 as (h & Hh & Hm).
+rewrite Nat.sub_diag in Hm.
+rewrite app_nil_r in Hm.
+...
+  cbn - [ seq ] in Hk.
+  rewrite (List_map_nth' 0) in Hk; [ | rewrite seq_length; flia ].
+  rewrite seq_nth in Hk; [ | flia ].
+  rewrite Nat.add_0_l in Hk.
+  destruct (Nat.eq_dec (vect_nat_el σ (S n)) (S n)) as [Hσn| Hσn]; [ easy | ].
+  destruct (Nat.eq_dec (vect_nat_el σ n) (S n)) as [Hσs| Hσs]. {
+    flia Hksn Hk.
+  }
+  subst k.
+...
+Search permut_fun_inv.
+...
   rewrite <- Hksn at 2.
   rewrite Hk.
   unfold permut_inv.
-  cbn - [ permut_fun_inv ].
+  cbn - [ seq ].
+  rewrite (List_map_nth' 0); [ | rewrite seq_length; flia ].
+  rewrite seq_nth; [ | flia ].
+  rewrite Nat.add_0_l.
+  destruct (Nat.eq_dec (vect_nat_el σ (S n)) (S n)) as [Hσn| Hσn]; [ easy | ].
+  destruct (Nat.eq_dec (vect_nat_el σ n) (S n)) as [Hσs| Hσs]. {
+...
+  destruct (Nat.eq_dec (vect_nat_el σ n) (S n)) as [H4| H4]; [ flia Hksn Hk | ].
+    admit.
+  }
+...
+  cbn - [ permut_fun_inv seq ].
+  rewrite (List_map_nth' 0); [ | rewrite seq_length; flia ].
+  rewrite seq_nth; [ | flia ].
+  rewrite Nat.add_0_l.
+  cbn.
+...
+  rewrite fun_find_permut_fun_inv'; [ | easy | flia ].
+  unfold permut_fun_inv'.
+  remember (Pigeonhole.pigeonhole_fun _ _) as x eqn:Hx.
+  symmetry in Hx.
+  destruct x as (x, x').
+  destruct (Nat.eq_dec x (S (S n))) as [Hxn| Hxn]. {
+    subst x.
+    apply Pigeonhole.pigeonhole with (b := S (S n)) in Hx; [ | flia | ]. 2: {
+      intros i Hi.
+      destruct (Nat.eq_dec i (S (S n))) as [Hin| Hin]; [ flia | ].
+      apply H1; flia Hi Hin.
+    }
+    destruct Hx as (_ & Hx'n & Hnx' & Hx').
+    destruct (Nat.eq_dec (S (S n)) (S (S n))) as [H| H]; [ clear H | easy ].
+    destruct (Nat.eq_dec x' (S (S n))) as [H| H]; [ now symmetry in H | ].
+    clear H.
+    rewrite <- Hksn, Hk.
+    unfold vect_nat_el.
+    cbn - [ seq ].
+    rewrite (List_map_nth' 0); [ | rewrite seq_length; flia ].
+    rewrite seq_nth; [ | flia ].
+    rewrite Nat.add_0_l.
+    destruct (Nat.eq_dec (vect_nat_el σ (S n)) (S n)) as [Hsn| Hsn]. {
+unfold vect_nat_el in Hsn.
+rewrite Hsn.
+...
+    rewrite Hx'.
+...
+Check fun_permut_fun_inv.
+...
   rewrite fun_permut_fun_inv; [ easy | easy | flia ].
 }
 specialize permut_inv_is_permut as H3.
