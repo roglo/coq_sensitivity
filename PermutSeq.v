f507fcb6d8a6caa559a5b4e54141fec6e84eb1bb
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-10-10 16:39:28 +0200

    -

--- PermutSeq.v
+++ PermutSeq.v
@@ -380,6 +380,9 @@ induction n; intros. {
   now specialize (Hinj H).
 }
 (**)
+(* https://fr.wikipedia.org/wiki/Groupe_sym%C3%A9trique#Propri%C3%A9t%C3%A9s *)
+Print vect_swap_elem.
+set (σ' := λ σ, vect_swap_elem 0 σ (S n) (vect_el 0 σ (S n)) ° σ).
 set (σ' := λ σ, mk_vect (map (λ i, if i =? S n then S n else vect_el 0 σ i) (seq 0 (S (S n))))).
 set (φ := λ σ, (vect_el 0 σ (S n), σ' σ)).
 ...
