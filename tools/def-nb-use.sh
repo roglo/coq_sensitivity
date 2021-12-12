68e22c9fc937916354a91b33b3dc8cbc72221a87
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-12-12 23:50:29 +0100

    -

--- tools/def-nb-use.sh
+++ tools/def-nb-use.sh
@@ -3,4 +3,4 @@
 #    returns the list of all theorems and definitions together with
 #    the number of their occurences, sorted by increasing number
 
-for i in $(cat def.txt); do grep -w $i *.v | echo "$(wc -l) $i"; done | LC_ALL=C sort -n
+for i in $(cat def.txt); do grep -w $i *.v | grep -v Arguments | echo "$(wc -l) $i"; done | LC_ALL=C sort -n
