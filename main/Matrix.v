03382b566ce948e0dab8def0894ffbd36b9713c6
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2021-12-13 15:45:09 +0100

    -

--- main/Matrix.v
+++ main/Matrix.v
@@ -2431,14 +2431,6 @@ rewrite map_length.
 now apply Hc.
 Qed.
 
-Theorem squ_mat_add_comm {n} : ∀ (MA MB : square_matrix n T),
-  (MA + MB)%F = (MB + MA)%F.
-Proof.
-intros.
-apply square_matrix_eq.
-apply mat_add_comm.
-Qed.
-
 Theorem squ_mat_add_assoc {n} : ∀ (MA MB MC : square_matrix n T),
   (MA + (MB + MC) = (MA + MB) + MC)%F.
 Proof.
@@ -3083,48 +3075,6 @@ Theorem squ_mat_consistent {n} :
 Proof.
 now split; right.
 Qed.
-
-Definition mat_ring_like_prop (n : nat) :
-  ring_like_prop (square_matrix n T) :=
-  {| rngl_is_comm := false;
-     rngl_has_dec_eq := @rngl_has_dec_eq T ro rp;
-     rngl_has_dec_le := false;
-     rngl_has_1_neq_0 := rngl_has_1_neq_0 && (n ≠? 0);
-     rngl_is_ordered := false;
-     rngl_is_integral := false;
-     rngl_characteristic := if n =? 0 then 1 else rngl_characteristic;
-     rngl_add_comm := squ_mat_add_comm;
-     rngl_add_assoc := squ_mat_add_assoc;
-     rngl_add_0_l := squ_mat_add_0_l;
-     rngl_mul_assoc := squ_mat_mul_assoc;
-     rngl_mul_1_l := squ_mat_mul_1_l;
-     rngl_mul_add_distr_l := squ_mat_mul_add_distr_l;
-     rngl_opt_1_neq_0 := squ_mat_opt_1_neq_0;
-     rngl_opt_mul_comm := NA;
-     rngl_opt_mul_1_r := squ_mat_mul_1_r;
-     rngl_opt_mul_add_distr_r := squ_mat_mul_add_distr_r;
-     rngl_opt_add_opp_l := squ_mat_opt_add_opp_l;
-     rngl_opt_add_sub := NA;
-     rngl_opt_sub_sub_sub_add := NA;
-     rngl_opt_mul_sub_distr_l := NA;
-     rngl_opt_mul_sub_distr_r := NA;
-     rngl_opt_mul_inv_l := NA;
-     rngl_opt_mul_inv_r := NA;
-     rngl_opt_mul_quot_l := NA;
-     rngl_opt_mul_quot_r := NA;
-     rngl_opt_eq_dec := squ_mat_opt_eq_dec;
-     rngl_opt_le_dec := NA;
-     rngl_opt_integral := NA;
-     rngl_characteristic_prop := squ_mat_characteristic_prop;
-     rngl_opt_le_refl := NA;
-     rngl_opt_le_antisymm := NA;
-     rngl_opt_le_trans := NA;
-     rngl_opt_add_le_compat := NA;
-     rngl_opt_mul_le_compat_nonneg := NA;
-     rngl_opt_mul_le_compat_nonpos := NA;
-     rngl_opt_mul_le_compat := NA;
-     rngl_opt_not_le := NA;
-     rngl_consistent := squ_mat_consistent |}.
 
 Theorem mat_vect_mul_0_r : ∀ m n (M : matrix T),
   m = mat_nrows M
