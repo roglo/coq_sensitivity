db0d06d9015190ed9748444c5163379cc410a73a
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2022-04-19 11:46:57 +0200

    -

--- main/PermutationFun.v
+++ main/PermutationFun.v
@@ -5,7 +5,7 @@ Set Nested Proofs Allowed.
 Set Implicit Arguments.
 *)
 
-Require Import Utf8 Arith (*Permutation*).
+Require Import Utf8 Arith Permutation.
 
 Import List List.ListNotations.
 (*
@@ -34,6 +34,14 @@ Fixpoint is_permutation {A} (eqb : A → A → bool) (la lb : list A) :=
       end
   end.
 
+Definition equality {A} (eqb : A → A → bool) := ∀ a b, eqb a b = true ↔ a = b.
+
+Theorem equality_refl {A} (eqb : A → _) : equality eqb → ∀ a, eqb a a = true.
+Proof.
+intros * Heqb *.
+now apply Heqb.
+Qed.
+
 Theorem extract_Some : ∀ A (f : A → _) l a bef aft,
   extract f l = Some (bef, a, aft) → f a = true ∧ l = bef ++ a :: aft.
 Proof.
@@ -53,3 +61,28 @@ specialize (IHl _ _ _ eq_refl) as H1.
 split; [ easy | ].
 now cbn; f_equal.
 Qed.
+
+(* to be completed
+Theorem Permutation_permutation : ∀ A (eqb : A → _) la lb,
+  equality eqb →
+  Permutation la lb
+  ↔ is_permutation eqb la lb = true.
+Proof.
+intros * Heqb.
+split. {
+  intros Hpab.
+  revert lb Hpab.
+  induction la as [| a]; intros; cbn. {
+    now apply Permutation_nil in Hpab; subst lb.
+  }
+  remember (extract (eqb a) lb) as lxl eqn:Hlxl; symmetry in Hlxl.
+  destruct lxl as [((bef, x), aft) | ]. {
+    apply extract_Some in Hlxl.
+    destruct Hlxl as (Hax, Hlb).
+    apply Heqb in Hax; subst x.
+    apply IHla.
+    apply Permutation_cons_app_inv with (a := a).
+    now rewrite <- Hlb.
+  }
+...
+*)
