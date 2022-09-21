c958e10a64664979d78f85b179e869cb8f78fada
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2022-09-21 04:26:01 +0200

    -

--- main/Polynomial.v
+++ main/Polynomial.v
@@ -995,6 +995,21 @@ destruct bef as [| c]. {
   now rewrite (equality_refl Heqb) in Hab.
 }
 cbn in Haft; injection Haft; clear Haft; intros; subst c lb.
+cbn in Hpab.
+(* ah, c'est désespérant *)
+...
+apply (permutation_sym Heqb) in Hpab.
+apply permutation_cons_l_iff in Hpab.
+remember (extract _ _) as lxl eqn:Hlxl; symmetry in Hlxl.
+destruct lxl as [((bef', x), aft')| ]; [ | easy ].
+apply extract_Some_iff in Hlxl.
+destruct Hlxl as (Hbef'' & H & Haft).
+apply Heqb in H; subst x.
+subst la.
+destruct bef as [| c]. {
+  cbn in Haft; injection Haft; intros; subst b aft.
+  now rewrite (equality_refl Heqb) in Hab.
+}
 ...
 specialize (IHla _ Hpab) as H1.
 assert (H : sorted leb ((b :: bef) ++ aft)). {
