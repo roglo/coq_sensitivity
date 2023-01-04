f466e2f8af686e934613ae7e1fba8013cc810670
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-01-04 09:39:07 +0100

    -

--- rngl_alg/Rrl.v
+++ rngl_alg/Rrl.v
@@ -196,7 +196,27 @@ Fixpoint tagada n :=
   | S n' => pouet (tagada n')
   end.
 
+Definition oups l :=
+  (if Nat.eqb (∑ (i = 1, length l), l.(i)) 0 then
+    [repeat 0 (S (length l))]
+   else []) ++
+  if Nat.eqb (length l) 1 then
+    [[S (hd 0 l)]]
+  else map (λ i, replace_at (i - 1) l (S (l.(i)))) (seq 1 (length l)).
+
+Definition gloup ll := map oups ll.
+
 (*
+Compute (let ll := [[]] in (pouet ll, gloup ll)).
+Compute (let ll := [[0]] in (pouet ll, gloup ll)).
+Compute (let ll := [[0; 0]; [1]] in (pouet ll, gloup ll)).
+Compute (let ll := [[0; 0; 0]; [0; 1]; [1; 0]; [2]] in (pouet ll, gloup ll)).
+...
+Compute (tagada 0).
+Compute (tagada 1).
+Compute (tagada 2).
+Compute (tagada 3).
+Compute (tagada 4).
 Compute (tagada 5).
 
 Compute (pouet [[]]).
