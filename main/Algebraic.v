7c645a8e1154ecddbcb4dc0cea1ae228132a7ce1
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-01-09 13:42:19 +0100

    -

--- main/Algebraic.v
+++ main/Algebraic.v
@@ -19,7 +19,14 @@ Context {ro : ring_like_op A}.
 Definition rlap_sylvester_list_list (rp rq : list A) : list (list A) :=
   let m := length rp in
   let n := length rq in
-  repeat (repeat 0%L (m + n)) (m + n). 
+  (rp ++ repeat 0%L n) ::
+  (repeat 0%L 1 ++ rp ++ repeat 0%L (n - 1)) ::
+  (repeat 0%L 2 ++ rp ++ repeat 0%L (n - 2)) ::
+  (repeat 0%L 3 ++ rp ++ repeat 0%L (n - 3)) ::
+  repeat (repeat 0%L (m + n)) (m - 4 + n).
+
+Definition rlap_sylvester_mat (rp rq : list A) : matrix A :=
+  mk_mat (rlap_sylvester_list_list rp rq).
 
 End a.
 
@@ -30,4 +37,6 @@ Require Import RnglAlg.Rational.
 Import Q.Notations.
 Open Scope Q_scope.
 
-Compute (rlap_sylvester_list_list [1;2;3;4] [5;6;7]).
+Compute (rlap_sylvester_mat [1;2;3;4] [5;6;7]).
+Compute (mat_nrows (rlap_sylvester_mat [1;2;3;4] [5;6;7])).
+Compute (mat_ncols (rlap_sylvester_mat [1;2;3;4] [5;6;7])).
