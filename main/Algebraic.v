02e6a83913ccd9013b1835904ee2264e53a05dec
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-01-09 15:48:51 +0100

    -

--- main/Algebraic.v
+++ main/Algebraic.v
@@ -16,23 +16,31 @@ Section a.
 Context {A : Type}.
 Context {ro : ring_like_op A}.
 
-Definition rlap_sylvester_list_list (rp rq : list A) : list (list A) :=
-  let m := length rp - 1 in
-  let n := length rq - 1 in
-  map (λ i, repeat 0%L i ++ rp ++ repeat 0%L (n - 1 - i)) (seq 0 n) ++
-  map (λ i, repeat 0%L i ++ rq ++ repeat 0%L (m - 1 - i)) (seq 0 m).
+Definition rlap_sylvester_list_list (rla rlb : list A) : list (list A) :=
+  let m := length rla - 1 in
+  let n := length rlb - 1 in
+  map (λ i, repeat 0%L i ++ rla ++ repeat 0%L (n - 1 - i)) (seq 0 n) ++
+  map (λ i, repeat 0%L i ++ rlb ++ repeat 0%L (m - 1 - i)) (seq 0 m).
 
-Definition rlap_sylvester_mat (rp rq : list A) : matrix A :=
-  mk_mat (rlap_sylvester_list_list rp rq).
+Definition rlap_sylvester_mat (rla rlb : list A) : matrix A :=
+  mk_mat (rlap_sylvester_list_list rla rlb).
+
+Definition polyn_sylvester_mat (p q : polyn A) : matrix A :=
+  mk_mat (rlap_sylvester_list_list (rev (lap p)) (rev (lap q))).
 
 End a.
 
-(*
+(**)
 Require Import RnglAlg.Qrl.
 Require Import RnglAlg.Rational.
 Import Q.Notations.
 Open Scope Q_scope.
 
+Check mk_polyn.
+Search has_polyn_prop.
+...
+
+Compute (polyn_sylvester_mat [1;2;3;4;5] [6;7;8;9]).
 Compute (rlap_sylvester_mat [1;2;3;4;5] [6;7;8;9]).
 Compute (mat_nrows (rlap_sylvester_mat [1;2;3;4;5] [6;7;8;9])).
 Compute (mat_ncols (rlap_sylvester_mat [1;2;3;4;5] [6;7;8;9])).
