9208b8524290ad339facd5abeefcfd685c3db6a4
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-01-09 14:46:06 +0100

    -

--- main/Algebraic.v
+++ main/Algebraic.v
@@ -19,10 +19,8 @@ Context {ro : ring_like_op A}.
 Definition rlap_sylvester_list_list (rp rq : list A) : list (list A) :=
   let m := length rp - 1 in
   let n := length rq - 1 in
-  (rp ++ repeat 0%L n) ::
-  map (λ i, repeat 0%L i ++ rp ++ repeat 0%L (n - 1 - i)) (seq 1 (n - 2)) ++
-  (rq ++ repeat 0%L m) ::
-  map (λ i, repeat 0%L i ++ rq ++ repeat 0%L (m - 1 - i)) (seq 0 (m - 2)).
+  map (λ i, repeat 0%L i ++ rp ++ repeat 0%L (n - 1 - i)) (seq 0 n) ++
+  map (λ i, repeat 0%L i ++ rq ++ repeat 0%L (m - 1 - i)) (seq 0 m).
 
 Definition rlap_sylvester_mat (rp rq : list A) : matrix A :=
   mk_mat (rlap_sylvester_list_list rp rq).
@@ -39,8 +37,8 @@ Compute (rlap_sylvester_mat [1;2;3;4;5] [6;7;8;9]).
 Compute (mat_nrows (rlap_sylvester_mat [1;2;3;4;5] [6;7;8;9])).
 Compute (mat_ncols (rlap_sylvester_mat [1;2;3;4;5] [6;7;8;9])).
 Compute (is_square_matrix (rlap_sylvester_mat [1;2;3;4;5] [6;7;8;9])).
-...
-Compute (rlap_sylvester_mat [1] [6]).
-Compute (rlap_sylvester_mat [1;2] [6;7]).
-Compute (rlap_sylvester_mat [1;2;3] [6;7;8]).
+Compute (let m := rlap_sylvester_mat [1;2;3;4] [6;7;8] in (mat_nrows m, mat_ncols m, m, is_square_matrix m)).
+Compute (let m := rlap_sylvester_mat [1;2;3] [6;7] in (mat_nrows m, mat_ncols m, m, is_square_matrix m)).
+Compute (let m := rlap_sylvester_mat [2] [6;7] in (mat_nrows m, mat_ncols m, m, is_square_matrix m)).
+Compute (let m := rlap_sylvester_mat [2;3] [6] in (mat_nrows m, mat_ncols m, m, is_square_matrix m)).
 *)
