e748d5f1abc7e8f5487d5f856a5473d0889757c9
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-01-19 20:04:44 +0100

    -

--- main/Algebraic.v
+++ main/Algebraic.v
@@ -673,8 +673,15 @@ Compute
 Compute
   (let roqp := Q_polyn_ring_like_op in
    let roq := Q_ring_like_op in
-   @mk_polyn (polyn Q) roqp [mk_polyn [0;1] eq_refl] eq_refl). (* z *)
-(* mmm... z-x est un polynôme à deux variables *)
+   @mk_polyn (polyn Q) roqp [polyn_zero; mk_polyn [1] eq_refl] eq_refl). (* z *)
+Compute
+  (let roqp := Q_polyn_ring_like_op in
+   let roq := Q_ring_like_op in
+   @mk_polyn (polyn Q) roqp [mk_polyn [0;-1] eq_refl] eq_refl). (* -x *)
+Compute
+  (let roqp := Q_polyn_ring_like_op in
+   let roq := Q_ring_like_op in
+   @mk_polyn (polyn Q) roqp [mk_polyn [0;-1] eq_refl; mk_polyn [1] eq_refl] eq_refl). (* z-x *)
 ...
 Compute
   (let roqp := Q_polyn_ring_like_op in
@@ -683,7 +690,6 @@ Compute
    let q := @mk_polyn _ roqp [mk_polyn [-2;0;1] eq_refl] eq_refl in
    @polyn_sylvester_mat _ roqp p q).
 (* oui, non, ça doit pas être ça... *)
-
 ...
 
 Check (@lap_compose Q Q_ring_like_op).
