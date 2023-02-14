9b2cb801e361789116132218ec515045c5f6448e
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-02-14 01:46:29 +0100

    -

--- main/Algebraic.v
+++ main/Algebraic.v
@@ -64,12 +64,14 @@ Definition glop_U (rla rlb : list T) :=
   let n := length rla - 1 in
   let m := length rlb - 1 in
   let s := rlap_sylvester_list_list rla rlb in
-  let s' := mk_mat (map (λ l, firstn (length l - 1) l) (tl s)) in
-  let s'' :=
-    mk_mat (map (λ l, firstn (length l - 1) l) (hd [] s :: tl (tl s)))
-  in
-  (repeat 0%L (m - 2) ++ [if even (m + n) then det s'' else (- det s'')%L] +
-   repeat 0%L (m - 1) ++ [if odd (m + n) then det s' else (- det s')%L])%lap.
+  ((let s'' :=
+      mk_mat (map (λ l, firstn (length l - 1) l) (hd [] s :: tl (tl s)))
+    in
+    repeat 0%L (m - 2) ++ [if even (m + n) then det s'' else (- det s'')%L]) +
+   (let s' :=
+      mk_mat (map (λ l, firstn (length l - 1) l) (tl s))
+    in
+    repeat 0%L (m - 1) ++ [if odd (m + n) then det s' else (- det s')%L]))%lap.
 
 End a.
 
