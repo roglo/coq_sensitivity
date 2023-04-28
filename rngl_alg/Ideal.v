f00c19c3d1575de7599621183d6e2f8a5c1bc10b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-04-28 18:47:10 +0200

    -

--- rngl_alg/Ideal.v
+++ rngl_alg/Ideal.v
@@ -11,40 +11,61 @@ Require Import Main.RingLike.
 
 Class ideal A {ro : ring_like_op A} := mk_I
   { i_type : A → bool;
-    i_prop :
-      ∀ a b, i_type a = true → i_type b = true → i_type (a * b)%L = true }.
+    i_prop_l : ∀ a b, i_type b = true → i_type (a * b)%L = true;
+    i_prop_r : ∀ a b, i_type a = true → i_type (a * b)%L = true }.
 
 Arguments ideal A%type {ro}.
 Arguments mk_I A%type {ro}.
 
-Theorem I_zero_prop A {ro : ring_like_op A} {rp : ring_like_prop A}
+Theorem I_zero_prop_l A {ro : ring_like_op A} {rp : ring_like_prop A}
+   (Hos : rngl_has_opp_or_subt = true) (Heb : rngl_has_eqb = true) :
+  ∀ (a b : A),
+  (b =? 0)%L = true
+  → ((a * b) =? 0)%L = true.
+Proof.
+intros * Hb.
+apply (rngl_eqb_eq Heb) in Hb.
+apply (rngl_eqb_eq Heb).
+subst b.
+apply (rngl_mul_0_r Hos).
+Qed.
+
+Theorem I_zero_prop_r A {ro : ring_like_op A} {rp : ring_like_prop A}
    (Hos : rngl_has_opp_or_subt = true) (Heb : rngl_has_eqb = true) :
   ∀ (a b : A),
   (a =? 0)%L = true
-  → (b =? 0)%L = true
   → ((a * b) =? 0)%L = true.
 Proof.
-intros * Ha Hb.
+intros * Ha.
 apply (rngl_eqb_eq Heb) in Ha.
 apply (rngl_eqb_eq Heb).
 subst a.
 apply (rngl_mul_0_l Hos).
 Qed.
 
-Arguments I_zero_prop A%type {ro rp} Hos Heb (a b)%L.
+Arguments I_zero_prop_l A%type {ro rp} Hos Heb (a b)%L.
+Arguments I_zero_prop_r A%type {ro rp} Hos Heb (a b)%L.
 
 Definition I_zero A {ro : ring_like_op A} {rp : ring_like_prop A}
     (Hos : rngl_has_opp_or_subt = true) (Heb : rngl_has_eqb = true) :
       ideal A :=
-  mk_I A (λ a, (a =? 0)%L) (I_zero_prop A Hos Heb).
+  mk_I A (λ a, (a =? 0)%L) (I_zero_prop_l A Hos Heb) (I_zero_prop_r A Hos Heb).
 
 Definition I_one A {ro : ring_like_op A} {rp : ring_like_prop A} :
       ideal A :=
-  mk_I A (λ a, true) (λ _ _ _ _, eq_refl).
+  mk_I A (λ a, true) (λ _ _ _, eq_refl) (λ _ _ _, eq_refl).
+
+(*
+Theorem I_add_prop_l :
+  ∀ A {ro : ring_like_op A} (a b : ideal A) (c d : A),
+  (i_type a || i_type b)%bool = true → (i_type a || i_type b)%bool = true.
+...
+*)
 
 Definition I_add A {ro : ring_like_op A} (a b : ideal A) : ideal A :=
-  mk_I (λ c, ...
-  (a + b)%L.
+  mk_I A (λ c, c = ...
+
+...
 
 Definition I_ring_like_op A (ro : ring_like_op A) (rp : ring_like_prop A)
     (Hos : rngl_has_opp_or_subt = true) (Heb : rngl_has_eqb = true) :
