01245507be89f53b892d5e5343b8aef1dd7824f1
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-04-28 11:30:22 +0200

    -

--- rngl_alg/Ideal.v
+++ rngl_alg/Ideal.v
@@ -15,9 +15,19 @@ Record ideal A {ro : ring_like_op A} := mk_I
       ∀ a b, i_type a = true → i_type b = true → i_type (a * b)%L = true }.
 
 Arguments ideal A%type {ro}.
+Arguments mk_I A%type {ro}.
+
+Definition glop A {ro : ring_like_op A} (a b : A) :
+  (rngl_eqb a =? 0)%L = true
+  → (rngl_eqb b =? 0)%L = true
+  → (rngl_eqb (a * b) =? 0)%L = true.
 
 Definition I_zero A {ro : ring_like_op A} : ideal A :=
-  mk_I (λ a, ...
+  mk_I A
+    (λ a, (rngl_eqb a =? 0)%L)
+    (glop A).
+
+...
 
 Definition I_ring_like_op A (ro : ring_like_op A) : ring_like_op (ideal A) :=
   {| rngl_zero := I_zero |}.
