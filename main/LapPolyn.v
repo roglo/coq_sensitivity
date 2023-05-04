24ff9d858b08f05f0d0e42f78f4faf5a957203aa
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-05-04 14:10:43 +0200

    -

---
+++ main/LapPolyn.v
@@ -0,0 +1,303 @@
+(* LapPolyn.v *)
+
+(* lists as polynomials ; the fact that the coefficient of the highest degree
+   is not zero is not tested. It is going to be tested in Polynomial.v. *)
+
+Set Nested Proofs Allowed.
+Set Implicit Arguments.
+
+Require Import Utf8 Arith.
+Import List ListNotations (*Init.Nat*).
+
+Require Import Misc RingLike IterAdd (*IterAnd*).
+(*
+Require Import PermutationFun SortingFun.
+*)
+
+(* (lap : list as polynomial) *)
+(* e.g. polynomial ax²+bx+c is implemented by the list [c;b;a] *)
+
+Section a.
+
+Context {T : Type}.
+Context (ro : ring_like_op T).
+Context (rp : ring_like_prop T).
+(*
+Context {Hos : rngl_has_opp_or_subt = true}.
+Context {Heb : rngl_has_eqb = true}.
+*)
+
+(* addition *)
+
+Definition lap_add la lb :=
+  map2 rngl_add
+    (la ++ repeat 0%L (length lb - length la))
+    (lb ++ repeat 0%L (length la - length lb)).
+
+Definition lap_subt la lb :=
+  map2 rngl_subt
+    (la ++ repeat 0%L (length lb - length la))
+    (lb ++ repeat 0%L (length la - length lb)).
+
+Definition lap_opp la := map rngl_opp la.
+Definition lap_sub la lb :=
+  if rngl_has_opp then lap_add la (lap_opp lb)
+  else if rngl_has_subt then lap_subt la lb
+  else repeat 0%L (max (length la) (length lb)).
+
+Theorem fold_lap_opp : ∀ la, map rngl_opp la = lap_opp la.
+Proof. easy. Qed.
+
+(* multiplication *)
+
+Fixpoint lap_convol_mul la lb i len :=
+  match len with
+  | O => []
+  | S len1 =>
+      (∑ (j = 0, i), List.nth j la 0 * List.nth (i - j) lb 0)%L ::
+      lap_convol_mul la lb (S i) len1
+  end.
+
+Definition lap_mul la lb :=
+  match la with
+  | [] => []
+  | _ =>
+      match lb with
+      | [] => []
+      | _ => lap_convol_mul la lb 0 (length la + length lb - 1)
+      end
+  end.
+
+(* power *)
+
+Fixpoint lap_power la n :=
+  match n with
+  | O => [1%L]
+  | S m => lap_mul la (lap_power la m)
+  end.
+
+(* euclidean division *)
+
+Fixpoint strip_0s la :=
+  match la with
+  | [] => []
+  | a :: la' => if (a =? 0)%L then strip_0s la' else la
+  end.
+
+Definition rlap_quot_rem_nb_iter (la lb : list T) :=
+  S (length la).
+
+Definition rlap_quot_rem_step (rla rlb : list T) :=
+  match rlb with
+  | [] => (None, []) (* division by zero *)
+  | b :: rlb' =>
+      match rla with
+      | [] => (None, [])
+      | a :: rla' =>
+          if length rla' <? length rlb' then (None, rla)
+          else
+            let cq := (a / b)%L in
+            let rlr := lap_sub rla' (map (λ cb, (cb * cq)%L) rlb') in
+            (Some cq, rlr)
+      end
+  end.
+
+Fixpoint rlap_quot_rem_loop it (rla rlb : list T) : list T * list T :=
+  match it with
+  | 0 => ([], [rngl_of_nat 97]) (* algo err: not enough iterations *)
+  | S it' =>
+      let (q, rlr) := rlap_quot_rem_step rla rlb in
+      match q with
+      | Some cq =>
+           let (rlq', rlr') := rlap_quot_rem_loop it' rlr rlb in
+           (cq :: rlq', rlr')
+      | None => ([], rlr)
+      end
+  end.
+
+Definition rlap_quot_rem rla rlb :=
+  rlap_quot_rem_loop (rlap_quot_rem_nb_iter rla rlb) rla rlb.
+
+Definition lap_quot_rem la lb :=
+  let (rlq, rlr) := rlap_quot_rem (rev la) (rev lb) in
+  (rev rlq, rev (strip_0s rlr)).
+
+Definition lap_quot la lb :=
+  let (rlq, rlr) := rlap_quot_rem (rev la) (rev lb) in
+  rev rlq.
+
+Definition lap_rem la lb :=
+  let (rlq, rlr) := rlap_quot_rem (rev la) (rev lb) in
+  rev (strip_0s rlr).
+
+(*
+End a.
+Arguments lap_add {T ro} (la lb)%list.
+Arguments lap_sub {T ro} (la lb)%list.
+Arguments lap_mul {T ro} (la lb)%list.
+Arguments lap_quot_rem {T ro} (la lb)%list.
+Arguments rlap_quot_rem {T ro} (rla rlb)%list.
+Arguments rlap_quot_rem_step {T ro} (rla rlb)%list.
+Arguments rlap_quot_rem_loop {T ro} it%nat (rla rlb)%list.
+Require Import RnglAlg.Qrl.
+Require Import RnglAlg.Rational.
+Import Q.Notations.
+Open Scope Q_scope.
+Compute (rlap_quot_rem [1] [2]).
+Compute (rlap_quot_rem [1;0;1;1;0] [1;0;1;0;0]).
+Compute (rlap_quot_rem [1;1;1] [1;1;0]).
+Compute (rlap_quot_rem [1;0;0;1] [1;1]).
+Compute (rlap_quot_rem [1;1;0;1] [1;1]).
+Compute (rlap_quot_rem_step [1;1;0;1] [1;1]).
+Compute (rlap_quot_rem [0;0;1] [1;1]).
+Compute (rlap_quot_rem_step [0;0;1] [1;1]).
+Print rlap_quot_rem_loop.
+Print rlap_quot_rem.
+Compute (rlap_quot_rem [0;1;0;0;1] [1;1]).
+Compute (
+  let la := [0;1] in
+  let lb := [1;0;0;1] in
+  let (lq, lr) := lap_quot_rem la lb in
+  (la = (lap_add (lap_mul lb lq) lr))).
+Compute (
+  let la := [1;0;0] in
+  let lb := [1;0;0;1] in
+  let (lq, lr) := lap_quot_rem la lb in
+  (la = (lap_add (lap_mul lb lq) lr))).
+Compute (rlap_quot_rem [1;0;0;1] [1;7]).
+Compute (rlap_quot_rem [6;-2;9;-2;-2] [1;0;2]).
+Compute (rlap_quot_rem [1;6;-1;-30] [1;5]).
+Compute (rlap_quot_rem [1;3;-2;7;-12] [1;0;-1]).
+Compute (rlap_quot_rem [1;0;1;-10] [1;-2]).
+Compute (rlap_quot_rem [1;-1;-1;3;-2;0] [1;-1;1]).
+Compute (rlap_quot_rem [1;1;1;1] [1;0;1]).
+Compute (rlap_quot_rem [1;2;0;3] [1;-1;-1]).
+Compute (rlap_quot_rem [1;2;0;3] [1;-1;-1]).
+Compute (rlap_quot_rem [1;0;0;-1;0] [2;1]).
+Compute (rlap_quot_rem [-3;0;1;0;-5;0;1] [3;1;1]).
+Compute (rlap_quot_rem [-3;0;1;0;-5;0;1;0;0] [3;1;1;0]).
+(* censé ci-dessous être (x3-2x+1, x-1) *)
+Compute (rlap_quot_rem [1;-1;-1;3;-2;0] [1;-1;1]).
+(**)
+Compute (lap_quot_rem [-2;-2;9;-2;6] [2;0;1]).
+Compute (lap_add (lap_mul [2;0;1] [-3;-2;6]) [4;2] = [-2;-2;9;-2;6]).
+...
+*)
+
+Theorem lap_add_0_l : ∀ la, lap_add [] la = la.
+Proof.
+intros; cbn.
+rewrite Nat.sub_0_r, app_nil_r.
+induction la as [| a]; [ easy | cbn ].
+now rewrite rngl_add_0_l; f_equal.
+Qed.
+
+Theorem lap_add_0_r : ∀ la, lap_add la [] = la.
+Proof.
+intros.
+unfold lap_add; cbn.
+rewrite Nat.sub_0_r, app_nil_r.
+induction la as [| a]; [ easy | cbn ].
+now rewrite rngl_add_0_r; f_equal.
+Qed.
+
+Theorem lap_mul_0_l : ∀ la, lap_mul [] la = [].
+Proof. easy. Qed.
+
+Theorem lap_mul_0_r : ∀ la, lap_mul la [] = [].
+Proof. now intros; destruct la. Qed.
+
+Theorem eq_lap_mul_0 : ∀ la lb, lap_mul la lb = [] → la = [] ∨ lb = [].
+Proof.
+intros * Hab.
+destruct la as [| a]; [ now left | right ].
+destruct lb as [| b]; [ easy | exfalso ].
+cbn in Hab.
+now rewrite Nat.add_succ_r in Hab.
+Qed.
+
+Theorem strip_0s_length_le : ∀ l, length (strip_0s l) ≤ length l.
+Proof.
+intros.
+induction l as [| a]; [ easy | cbn ].
+destruct (a =? 0)%L; cbn; [ | easy ].
+flia IHl.
+Qed.
+
+(* could be moved to Misc.v *)
+Theorem min_add_sub_max : ∀ a b, min (a + (b - a)) (b + (a - b)) = max a b.
+Proof.
+intros.
+destruct (le_dec a b) as [Hab| Hab]. {
+  rewrite Nat.add_comm, Nat.sub_add; [ | easy ].
+  rewrite (proj2 (Nat.sub_0_le _ _) Hab).
+  rewrite Nat.add_0_r, Nat.min_id; symmetry.
+  now apply Nat.max_r.
+} {
+  apply Nat.nle_gt, Nat.lt_le_incl in Hab.
+  rewrite Nat.min_comm, Nat.max_comm.
+  rewrite Nat.add_comm, Nat.sub_add; [ | easy ].
+  rewrite (proj2 (Nat.sub_0_le _ _) Hab).
+  rewrite Nat.add_0_r, Nat.min_id; symmetry.
+  now apply Nat.max_r.
+}
+Qed.
+
+Theorem lap_add_length : ∀ la lb,
+  length (lap_add la lb) = max (length la) (length lb).
+Proof.
+intros.
+unfold lap_add.
+rewrite map2_length.
+do 2 rewrite app_length, repeat_length.
+apply min_add_sub_max.
+Qed.
+
+Theorem lap_subt_length : ∀ la lb,
+  length (lap_subt la lb) = max (length la) (length lb).
+Proof.
+intros.
+unfold lap_subt.
+rewrite map2_length.
+do 2 rewrite app_length, repeat_length.
+apply min_add_sub_max.
+Qed.
+
+Theorem lap_opp_length : ∀ la, length (lap_opp la) = length la.
+Proof.
+intros.
+induction la as [| a]; [ easy | cbn ].
+f_equal; apply IHla.
+Qed.
+
+Theorem lap_sub_length : ∀ la lb,
+  length (lap_sub la lb) = max (length la) (length lb).
+Proof.
+intros.
+unfold lap_sub.
+destruct rngl_has_opp; [ now rewrite lap_add_length, lap_opp_length | ].
+destruct rngl_has_subt; [ now rewrite lap_subt_length | ].
+now rewrite repeat_length.
+Qed.
+
+End a.
+
+Declare Scope lap_scope.
+Delimit Scope lap_scope with lap.
+
+Arguments lap_add {T ro} (la lb)%lap.
+Arguments lap_add_length {T ro} (la lb)%lap.
+Arguments lap_add_0_l {T ro rp} la%lap.
+Arguments lap_add_0_r {T ro rp} la%lap.
+Arguments lap_convol_mul {T ro} (la lb)%lap (i len)%nat.
+Arguments lap_mul {T ro} (la lb)%lap.
+Arguments lap_opp {T ro} la%lap.
+Arguments lap_quot {T ro} (la lb)%lap.
+Arguments lap_quot_rem {T ro} (la lb)%lap.
+Arguments lap_rem {T ro} (la lb)%lap.
+Arguments lap_sub {T ro} (la lb)%lap.
+Arguments lap_subt {T ro} (la lb)%lap.
+Arguments rlap_quot_rem {T ro} (rla rlb)%list.
+Arguments rlap_quot_rem_loop {T ro} it%nat (rla rlb)%list.
+Arguments rlap_quot_rem_step {T ro} (rla rlb)%list.
+Arguments strip_0s {T ro} la%list.
--- main/Polynomial.v
+++ main/Polynomial.v
@@ -10,6 +10,7 @@ Import List ListNotations Init.Nat.
 
 Require Import Misc RingLike IterAdd IterAnd.
 Require Import PermutationFun SortingFun.
+Require Import LapPolyn.
 
 (* (lap : list as polynomial) *)
 (* e.g. polynomial ax²+bx+c is implemented by the list [c;b;a] *)
@@ -43,24 +44,9 @@ Fixpoint lap_eqb zero (eqb : T → _) (la lb : list T) :=
       end
   end.
 
-(*
-End a.
-Require Import RnglAlg.Qrl.
-Require Import RnglAlg.Rational.
-Import Q.Notations.
-Open Scope Q_scope.
-Compute (lap_eqb 0%Q Q.eqb [1;0;0;0] [1;0;3]).
-*)
-
 Definition lap_one := [1%L].
 
 (* normalization *)
-
-Fixpoint strip_0s la :=
-  match la with
-  | [] => []
-  | a :: la' => if (a =? 0)%L then strip_0s la' else la
-  end.
 
 Lemma strip_0s_app : ∀ la lb,
   strip_0s (la ++ lb) =
@@ -168,258 +154,6 @@ rewrite last_last.
 destruct IHla as [H1| H1]; [ | easy ].
 apply is_empty_list_empty in H1.
 now apply app_eq_nil in H1.
-Qed.
-
-(* addition *)
-
-Definition lap_add la lb :=
-  map2 rngl_add
-    (la ++ repeat 0%L (length lb - length la))
-    (lb ++ repeat 0%L (length la - length lb)).
-
-Definition lap_subt la lb :=
-  map2 rngl_subt
-    (la ++ repeat 0%L (length lb - length la))
-    (lb ++ repeat 0%L (length la - length lb)).
-
-Definition lap_opp la := map rngl_opp la.
-Definition lap_sub la lb :=
-  if rngl_has_opp then lap_add la (lap_opp lb)
-  else if rngl_has_subt then lap_subt la lb
-  else repeat 0%L (max (length la) (length lb)).
-
-Theorem fold_lap_opp : ∀ la, map rngl_opp la = lap_opp la.
-Proof. easy. Qed.
-
-(*
-Theorem fold_lap_sub : ∀ la lb, lap_add la (lap_opp lb) = lap_sub la lb.
-Proof. easy. Qed.
-*)
-
-(* multiplication *)
-
-Fixpoint lap_convol_mul la lb i len :=
-  match len with
-  | O => []
-  | S len1 =>
-      (∑ (j = 0, i), List.nth j la 0 * List.nth (i - j) lb 0)%L ::
-      lap_convol_mul la lb (S i) len1
-  end.
-
-Definition lap_mul la lb :=
-  match la with
-  | [] => []
-  | _ =>
-      match lb with
-      | [] => []
-      | _ => lap_convol_mul la lb 0 (length la + length lb - 1)
-      end
-  end.
-
-(* power *)
-
-Fixpoint lap_power la n :=
-  match n with
-  | O => [1%L]
-  | S m => lap_mul la (lap_power la m)
-  end.
-
-(* euclidean division *)
-
-Definition rlap_quot_rem_nb_iter (la lb : list T) :=
-  S (length la).
-
-Definition rlap_quot_rem_step (rla rlb : list T) :=
-  match rlb with
-  | [] => (None, []) (* division by zero *)
-  | b :: rlb' =>
-      match rla with
-      | [] => (None, [])
-      | a :: rla' =>
-          if length rla' <? length rlb' then (None, rla)
-          else
-            let cq := (a / b)%L in
-            let rlr := lap_sub rla' (map (λ cb, (cb * cq)%L) rlb') in
-            (Some cq, rlr)
-      end
-  end.
-
-Fixpoint rlap_quot_rem_loop it (rla rlb : list T) : list T * list T :=
-  match it with
-  | 0 => ([], [rngl_of_nat 97]) (* algo err: not enough iterations *)
-  | S it' =>
-      let (q, rlr) := rlap_quot_rem_step rla rlb in
-      match q with
-      | Some cq =>
-           let (rlq', rlr') := rlap_quot_rem_loop it' rlr rlb in
-           (cq :: rlq', rlr')
-      | None => ([], rlr)
-      end
-  end.
-
-Definition rlap_quot_rem rla rlb :=
-  rlap_quot_rem_loop (rlap_quot_rem_nb_iter rla rlb) rla rlb.
-
-Definition lap_quot_rem la lb :=
-  let (rlq, rlr) := rlap_quot_rem (rev la) (rev lb) in
-  (rev rlq, rev (strip_0s rlr)).
-
-Definition lap_quot la lb :=
-  let (rlq, rlr) := rlap_quot_rem (rev la) (rev lb) in
-  rev rlq.
-
-Definition lap_rem la lb :=
-  let (rlq, rlr) := rlap_quot_rem (rev la) (rev lb) in
-  rev (strip_0s rlr).
-
-(*
-End a.
-Arguments lap_add {T ro} (la lb)%list.
-Arguments lap_sub {T ro} (la lb)%list.
-Arguments lap_mul {T ro} (la lb)%list.
-Arguments lap_quot_rem {T ro} (la lb)%list.
-Arguments rlap_quot_rem {T ro} (rla rlb)%list.
-Arguments rlap_quot_rem_step {T ro} (rla rlb)%list.
-Arguments rlap_quot_rem_loop {T ro} it%nat (rla rlb)%list.
-Require Import RnglAlg.Qrl.
-Require Import RnglAlg.Rational.
-Import Q.Notations.
-Open Scope Q_scope.
-Compute (rlap_quot_rem [1] [2]).
-Compute (rlap_quot_rem [1;0;1;1;0] [1;0;1;0;0]).
-Compute (rlap_quot_rem [1;1;1] [1;1;0]).
-Compute (rlap_quot_rem [1;0;0;1] [1;1]).
-Compute (rlap_quot_rem [1;1;0;1] [1;1]).
-Compute (rlap_quot_rem_step [1;1;0;1] [1;1]).
-Compute (rlap_quot_rem [0;0;1] [1;1]).
-Compute (rlap_quot_rem_step [0;0;1] [1;1]).
-Print rlap_quot_rem_loop.
-Print rlap_quot_rem.
-Compute (rlap_quot_rem [0;1;0;0;1] [1;1]).
-Compute (
-  let la := [0;1] in
-  let lb := [1;0;0;1] in
-  let (lq, lr) := lap_quot_rem la lb in
-  (la = (lap_add (lap_mul lb lq) lr))).
-Compute (
-  let la := [1;0;0] in
-  let lb := [1;0;0;1] in
-  let (lq, lr) := lap_quot_rem la lb in
-  (la = (lap_add (lap_mul lb lq) lr))).
-Compute (rlap_quot_rem [1;0;0;1] [1;7]).
-Compute (rlap_quot_rem [6;-2;9;-2;-2] [1;0;2]).
-Compute (rlap_quot_rem [1;6;-1;-30] [1;5]).
-Compute (rlap_quot_rem [1;3;-2;7;-12] [1;0;-1]).
-Compute (rlap_quot_rem [1;0;1;-10] [1;-2]).
-Compute (rlap_quot_rem [1;-1;-1;3;-2;0] [1;-1;1]).
-Compute (rlap_quot_rem [1;1;1;1] [1;0;1]).
-Compute (rlap_quot_rem [1;2;0;3] [1;-1;-1]).
-Compute (rlap_quot_rem [1;2;0;3] [1;-1;-1]).
-Compute (rlap_quot_rem [1;0;0;-1;0] [2;1]).
-Compute (rlap_quot_rem [-3;0;1;0;-5;0;1] [3;1;1]).
-Compute (rlap_quot_rem [-3;0;1;0;-5;0;1;0;0] [3;1;1;0]).
-(* censé ci-dessous être (x3-2x+1, x-1) *)
-Compute (rlap_quot_rem [1;-1;-1;3;-2;0] [1;-1;1]).
-(**)
-Compute (lap_quot_rem [-2;-2;9;-2;6] [2;0;1]).
-Compute (lap_add (lap_mul [2;0;1] [-3;-2;6]) [4;2] = [-2;-2;9;-2;6]).
-...
-*)
-
-Theorem lap_add_0_l : ∀ la, lap_add [] la = la.
-Proof.
-intros; cbn.
-rewrite Nat.sub_0_r, app_nil_r.
-induction la as [| a]; [ easy | cbn ].
-now rewrite rngl_add_0_l; f_equal.
-Qed.
-
-Theorem lap_add_0_r : ∀ la, lap_add la [] = la.
-Proof.
-intros.
-unfold lap_add; cbn.
-rewrite Nat.sub_0_r, app_nil_r.
-induction la as [| a]; [ easy | cbn ].
-now rewrite rngl_add_0_r; f_equal.
-Qed.
-
-Theorem lap_mul_0_l : ∀ la, lap_mul [] la = [].
-Proof. easy. Qed.
-
-Theorem lap_mul_0_r : ∀ la, lap_mul la [] = [].
-Proof. now intros; destruct la. Qed.
-
-Theorem eq_lap_mul_0 : ∀ la lb, lap_mul la lb = [] → la = [] ∨ lb = [].
-Proof.
-intros * Hab.
-destruct la as [| a]; [ now left | right ].
-destruct lb as [| b]; [ easy | exfalso ].
-cbn in Hab.
-now rewrite Nat.add_succ_r in Hab.
-Qed.
-
-Theorem strip_0s_length_le : ∀ l, length (strip_0s l) ≤ length l.
-Proof.
-intros.
-induction l as [| a]; [ easy | cbn ].
-destruct (a =? 0)%L; cbn; [ | easy ].
-flia IHl.
-Qed.
-
-(* could be moved to Misc.v *)
-Theorem min_add_sub_max : ∀ a b, min (a + (b - a)) (b + (a - b)) = max a b.
-Proof.
-intros.
-destruct (le_dec a b) as [Hab| Hab]. {
-  rewrite Nat.add_comm, Nat.sub_add; [ | easy ].
-  rewrite (proj2 (Nat.sub_0_le _ _) Hab).
-  rewrite Nat.add_0_r, Nat.min_id; symmetry.
-  now apply Nat.max_r.
-} {
-  apply Nat.nle_gt, Nat.lt_le_incl in Hab.
-  rewrite Nat.min_comm, Nat.max_comm.
-  rewrite Nat.add_comm, Nat.sub_add; [ | easy ].
-  rewrite (proj2 (Nat.sub_0_le _ _) Hab).
-  rewrite Nat.add_0_r, Nat.min_id; symmetry.
-  now apply Nat.max_r.
-}
-Qed.
-
-Theorem lap_add_length : ∀ la lb,
-  length (lap_add la lb) = max (length la) (length lb).
-Proof.
-intros.
-unfold lap_add.
-rewrite map2_length.
-do 2 rewrite app_length, repeat_length.
-apply min_add_sub_max.
-Qed.
-
-Theorem lap_subt_length : ∀ la lb,
-  length (lap_subt la lb) = max (length la) (length lb).
-Proof.
-intros.
-unfold lap_subt.
-rewrite map2_length.
-do 2 rewrite app_length, repeat_length.
-apply min_add_sub_max.
-Qed.
-
-Theorem lap_opp_length : ∀ la, length (lap_opp la) = length la.
-Proof.
-intros.
-induction la as [| a]; [ easy | cbn ].
-f_equal; apply IHla.
-Qed.
-
-Theorem lap_sub_length : ∀ la lb,
-  length (lap_sub la lb) = max (length la) (length lb).
-Proof.
-intros.
-unfold lap_sub.
-destruct rngl_has_opp; [ now rewrite lap_add_length, lap_opp_length | ].
-destruct rngl_has_subt; [ now rewrite lap_subt_length | ].
-now rewrite repeat_length.
 Qed.
 
 Theorem all_0_lap_norm_nil : ∀ la,
@@ -801,15 +535,6 @@ Definition lap_ring_like_op : ring_like_op (list T) :=
      rngl_opt_inv_or_quot := lap_opt_inv_or_quot;
      rngl_opt_eqb := Some (lap_eqb rngl_zero rngl_eqb);
      rngl_opt_le := None |}.
-
-Declare Scope lap_scope.
-Delimit Scope lap_scope with lap.
-
-Arguments lap_add (la lb)%lap.
-Arguments lap_mul (la lb)%lap.
-Arguments lap_quot_rem (la lb)%lap.
-Arguments lap_quot (la lb)%lap.
-Arguments lap_rem (la lb)%lap.
 
 Notation "1" := lap_one : lap_scope.
 Notation "- a" := (lap_opp a) : lap_scope.
@@ -2805,8 +2530,17 @@ revert len Hlen.
 induction la as [| a]; intros. {
   now apply Nat.le_0_r in Hlen; subst len.
 }
+(*
 cbn.
+destruct len. {
+  cbn - [ lap_add ].
+  now rewrite lap_add_0_l.
+}
+cbn.
+*)
 destruct len; [ now rewrite lap_add_0_l | cbn ].
+...
+(**)
 cbn in Hlen; apply Nat.succ_le_mono in Hlen.
 rewrite rngl_add_0_l; f_equal.
 now apply IHla.
@@ -4042,18 +3776,13 @@ Arguments eval_rlap {T ro} rla%lap x%L.
 Arguments has_polyn_prop {T ro} la%lap.
 Arguments has_polyn_prop_lap_norm {T ro rp} Heb la%lap.
 
-Arguments lap_add {T ro} (la lb)%lap.
 Arguments lap_add_move_l {T ro rp} Hos (la lb lc)%lap.
 Arguments lap_add_norm_idemp_l {T ro rp} Heb (la lb)%lap.
 Arguments lap_add_norm_idemp_r {T ro rp} Heb (la lb)%lap.
-Arguments lap_add_0_l {T ro rp} la%lap.
-Arguments lap_add_0_r {T ro rp} la%lap.
 Arguments lap_add_assoc {T ro rp} (al1 al2 al3)%lap.
 Arguments lap_add_app_l {T ro rp} (la lb lc)%lap.
 Arguments lap_add_app_r {T ro rp} (la lb lc)%lap.
 Arguments lap_compose {T ro} (la lb)%lap.
-Arguments lap_convol_mul {T ro} (la lb)%lap (i len)%nat.
-Arguments lap_mul {T ro} (la lb)%lap.
 Arguments lap_mul_add_distr_l {T ro rp} Hos Heb (la lb lc)%lap.
 Arguments lap_mul_comm {T ro rp} Hic (a b)%lap.
 Arguments lap_mul_div {T ro rp} Hos Heb Hic Hop Hiv (la lb)%lap.
@@ -4068,16 +3797,10 @@ Arguments lap_norm_length_le {T ro rp} Heb la%lap.
 Arguments lap_norm_mul {T ro rp} Hos Heb Hiv (la lb)%lap.
 Arguments lap_norm_repeat_0 {T ro rp} Heb n%nat.
 Arguments lap_one {T ro}.
-Arguments lap_opp {T ro} la%lap.
-Arguments lap_quot {T ro} (la lb)%lap.
 Arguments lap_quot_is_norm {T ro rp} Hos Heb Hiv (la lb)%lap.
-Arguments lap_quot_rem {T ro} (la lb)%lap.
 Arguments lap_quot_rem_prop {T ro rp} Hos Heb Hic Hop Hiv (la lb lq lr)%lap.
-Arguments lap_rem {T ro} (la lb)%lap.
 Arguments lap_rem_is_norm {T ro rp} Heb (la lb)%lap.
 Arguments lap_ring_like_op {T ro}.
-Arguments lap_sub {T ro} (la lb)%lap.
-Arguments lap_subt {T ro} (la lb)%lap.
 Arguments lap_subt_diag {T ro rp} Hos la%lap.
 Arguments lap_subt_0_r {T ro rp} Hsu la%lap.
 Arguments lap_x_power {T ro} n%nat.
@@ -4088,9 +3811,6 @@ Arguments map2_rngl_add_0_r {T ro rp} la%lap.
 Arguments map2_rngl_subt_0_r {T ro rp} Hsu la%lap.
 Arguments polyn_norm_prop {T ro} la%lap.
 Arguments rlap_compose {T ro} (rla rlb)%lap.
-Arguments rlap_quot_rem {T ro} (rla rlb)%lap.
-Arguments rlap_quot_rem_loop {T ro} it%nat (rla rlb)%lap.
-Arguments strip_0s {T ro} la%lap.
 
 Notation "1" := lap_one : lap_scope.
 Notation "- a" := (lap_opp a) : lap_scope.
@@ -5569,12 +5289,6 @@ Require Import RnglAlg.Qrl.
 Require Import RnglAlg.Rational.
 Import Q.Notations.
 Open Scope Q_scope.
-Arguments lap_add {T ro} (la lb)%list.
-Arguments lap_sub {T ro} (la lb)%list.
-Arguments lap_mul {T ro} (la lb)%list.
-Arguments lap_quot_rem {T ro} (la lb)%list.
-Arguments rlap_quot_rem {T ro} (rla rlb)%list.
-Arguments rlap_quot_rem_step {T ro} (rla rlb)%list.
 Arguments rlap_quot_rem_loop {T ro} it%nat (rla rlb)%list.
 Compute (
   let qro := Q_ring_like_op in
