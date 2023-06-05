50b0336b7193062b6f9ac38051d7524ac98c5570
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-06-05 10:01:01 +0200

    -

--- rngl_alg/Rrl.v
+++ rngl_alg/Rrl.v
@@ -202,6 +202,7 @@ Arguments mk_c {T} re%L im%L.
 Arguments re {T} complex%L.
 Arguments im {T} complex%L.
 
+Arguments rngl_has_dec_le T {ro ring_like_prop}.
 Arguments rngl_has_eqb T {R}.
 Arguments rngl_has_opp T {R}.
 Arguments rngl_has_opp_or_subt T {R}.
@@ -607,11 +608,12 @@ Theorem complex_opt_mul_inv_l {T}
   rngl_mul_is_comm T = true →
   rngl_has_eqb T = true →
   rngl_is_ordered T = true →
+  rngl_has_dec_le T = true →
   if (rngl_has_inv (complex T) && rngl_has_1 (complex T))%bool then
     ∀ a : complex T, a ≠ 0%L → (a⁻¹ * a)%L = 1%L
   else not_applicable.
 Proof.
-intros * Hop Hic Heb Hor.
+intros * Hop Hic Heb Hor Hdl.
 remember (rngl_has_inv (complex T)) as ivc eqn:Hivc; symmetry in Hivc.
 destruct ivc; [ | easy ].
 remember (rngl_has_1 (complex T)) as onc eqn:Honc; symmetry in Honc.
@@ -647,7 +649,14 @@ rewrite (rngl_mul_comm Hic), H1. 2: {
   intros Hri.
   destruct a as (ra, ia).
   cbn in Hri.
-  apply (rngl_eq_add_0 Hor) in Hri.
+  apply (rngl_eq_add_0 Hor) in Hri; cycle 1. {
+    destruct (rngl_le_dec Hdl 0%L ra) as [Hap| Han]. {
+      specialize rngl_opt_mul_le_compat_nonneg as H2.
+      rewrite Hor, Hop in H2; cbn in H2.
+      specialize (H2 0%L 0%L ra ra).
+Search rngl_is_ordered.
+Check rngl_le_dec.
+...
 Search (_ ≤ _ * _)%L.
 ...
   apply (neq_complex_neq Heb) in Haz.
