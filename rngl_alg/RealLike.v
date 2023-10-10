e994158c5a7e04fa8ca8941608827d2e05765100
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-10-10 08:37:38 +0200

    -

--- rngl_alg/RealLike.v
+++ rngl_alg/RealLike.v
@@ -3467,6 +3467,95 @@ destruct aov. 2: {
                 apply (rngl_nle_gt Hor) in Hzc2.
                 (* here, for sin θ3 to be non negative, then the negativity
                    of θ2 must not be greater than the positivity of θ1 *)
+apply (rngl_le_sub_le_add_r Hop Hor).
+rewrite (rngl_sub_0_l Hop).
+apply (rngl_nlt_ge Hor).
+intros Hcc.
+apply (rngl_nlt_ge Hor) in Hzs3.
+apply Hzs3; clear Hzs3.
+subst θ3; cbn.
+(**)
+(* what happens if sin θ2 = 0 ? *)
+destruct (rngl_eq_dec Hed (rngl_sin θ2) 0) as [H2z| H2z]. {
+  rewrite H2z, (rngl_mul_0_r Hos), rngl_add_0_l.
+  destruct (rngl_eq_dec Hed (rngl_sin θ1) 0) as [H1z| H1z]. {
+    apply (eq_rngl_sin_0 Hic Hon Hop Hed) in H2z, H1z.
+    destruct H2z as [H2z| H2z]. {
+      subst θ2.
+      apply (rngl_nle_gt Hor) in Hzc2.
+      exfalso; apply Hzc2; clear Hzc2; cbn.
+      apply (rngl_0_le_1 Hon Hop Hor).
+    }
+    subst θ2.
+    clear Hzs2 Hzc2.
+    cbn in Hcc.
+    exfalso.
+    destruct H1z as [H1z| H1z]. {
+      subst θ1.
+      rewrite (rngl_opp_involutive Hop) in Hcc.
+      cbn in Hcc.
+      now apply (rngl_lt_irrefl Hor) in Hcc.
+    } {
+      subst θ1.
+      cbn in Hzc1.
+      apply (rngl_nlt_ge Hor) in Hzc1.
+      apply Hzc1; clear Hzc1.
+      apply (rngl_opp_lt_compat Hop Hor).
+      rewrite (rngl_opp_0 Hop).
+      rewrite (rngl_opp_involutive Hop).
+      apply (rngl_0_lt_1 Hon Hop Hc1 Hor).
+    }
+  }
+  assert (Hzls2 : (0 < rngl_sin θ2)%L). {
+    apply (rngl_lt_iff Hor).
+    split; [ easy | ].
+    apply not_eq_sym.
+...
+    progress unfold angle_ltb in Haov.
+    cbn in Haov.
+...
+      apply angle_leb_gt in Haov.
+      apply Bool.not_true_iff_false in Haov.
+      exfalso; apply Haov; clear Haov.
+
+      apply angle_nle_gt in Haov.
+...
+enough (Hzls2 : (0 < rngl_sin θ2)%L).
+...
+(* 1st option : sin θ2 < sin θ1 *)
+enough (Hs21 : (rngl_sin θ2 < rngl_sin θ1)%L).
+apply (rngl_le_lt_trans Hor _ ((- rngl_cos θ2) * rngl_sin θ2 + rngl_sin θ1 * rngl_cos θ2))%L. {
+  apply (rngl_add_le_mono_r Hop Hor).
+  apply (rngl_mul_le_mono_pos_r Hop Hor Hii).
+easy.
+now apply (rngl_lt_le_incl Hor).
+} {
+  rewrite rngl_add_comm.
+  rewrite (rngl_mul_comm Hic).
+  rewrite (rngl_mul_opp_l Hop).
+  rewrite (fold_rngl_sub Hop).
+  rewrite <- (rngl_mul_sub_distr_l Hop).
+rewrite (rngl_mul_comm Hic).
+  apply (rngl_mul_pos_neg Hop Hor Hid); [ | easy ].
+  now apply (rngl_lt_0_sub Hop Hor).
+}
+...
+(* 2nd option : sin θ2 ≤ sin θ1 *)
+enough (Hs21 : (rngl_sin θ2 ≤ rngl_sin θ1)%L).
+apply (rngl_lt_le_trans Hor _ ((- rngl_cos θ2) * rngl_sin θ2 + rngl_sin θ1 * rngl_cos θ2))%L. {
+  apply (rngl_add_lt_mono_r Hop Hor).
+  now apply (rngl_mul_lt_mono_pos_r Hop Hor Hii).
+} {
+  rewrite rngl_add_comm.
+  rewrite (rngl_mul_comm Hic).
+  rewrite (rngl_mul_opp_l Hop).
+  rewrite (fold_rngl_sub Hop).
+  rewrite <- (rngl_mul_sub_distr_l Hop).
+  apply (rngl_mul_nonpos_nonneg Hop Hor). {
+    now apply (rngl_lt_le_incl Hor).
+  }
+  now apply (rngl_le_0_sub Hop Hor).
+}
 ...
   remember (θ1 + θ2)%A as θ3 eqn:Hθ3.
   specialize (rngl_0_lt_2 Hon Hop Hc1 Hor) as Hz2.
