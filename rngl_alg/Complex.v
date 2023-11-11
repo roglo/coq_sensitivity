16bead146ee436ff90581704df45dfe51b4a668b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2023-11-11 14:38:34 +0100

    -

--- rngl_alg/Complex.v
+++ rngl_alg/Complex.v
@@ -4369,50 +4369,63 @@ destruct zs23. {
           rewrite (rngl_sin_sub_right_l Hon Hos) in Hc3z |-*.
           rewrite (rngl_cos_sub_right_l Hon Hop) in Hzs3, Hzs23.
           move Hzs3 before Hzs2; move Hc3z after Hzc2.
+          destruct (rngl_le_dec Hor (rngl_cos θ2) 0) as [H| H]. {
+            apply (rngl_le_antisymm Hor) in H; [ | easy ].
+            symmetry in H.
+            apply (eq_rngl_cos_0 Hic Hon Hop Hed) in H.
+            destruct H; subst θ2. {
+              rewrite (rngl_sin_sub_right_r Hon Hop) in Hzs23.
+              apply (rngl_opp_nonneg_nonpos Hop Hor) in Hzs23.
+              now apply (rngl_nlt_ge Hor) in Hzs23.
+            }
+            exfalso.
+            apply (rngl_nle_gt Hor) in Hzs2.
+            apply Hzs2; clear Hzs2; cbn.
+            apply (rngl_opp_nonpos_nonneg Hop Hor).
+            apply (rngl_0_le_1 Hon Hop Hor).
+          }
+          apply (rngl_nle_gt Hor) in H.
+          move H before Hzc2; clear Hzc2; rename H into Hzc2.
           enough (H : (rngl_sin θ3 * rngl_cos θ2 < rngl_sin θ1)%L). {
             cbn in Hzs12, Hzs23.
             rewrite (rngl_mul_opp_r Hop) in Hzs12, Hzs23.
             rewrite (rngl_add_opp_l Hop) in Hzs12, Hzs23.
-            apply -> (rngl_le_0_sub Hop) in Hzs12.
-            apply -> (rngl_le_0_sub Hop) in Hzs23.
+            apply -> (rngl_le_0_sub Hop Hor) in Hzs12.
+            apply -> (rngl_le_0_sub Hop Hor) in Hzs23.
             remember (rngl_cos θ2 * rngl_cos θ1 * rngl_sin θ2)%L as x.
             apply (rngl_add_lt_mono_r Hop Hor _ _ x) in H.
             subst x.
-            apply
-              (rngl_lt_trans Hor _
-                 (rngl_sin θ3 * rngl_sin θ2 + ...
-...
-Search (rngl_cos _ < rngl_cos _)%L.
-eapply rngl_cos_cos_sin_sin_nonneg_sin_le_cos_le_iff.
-...
-          cbn in Hzs12, Hzs23 |-*.
-          rewrite (rngl_mul_opp_r Hop) in Hzs12.
-          rewrite (rngl_add_opp_l Hop) in Hzs12.
-          apply -> (rngl_le_0_sub Hop Hor) in Hzs12.
-          apply -> (rngl_le_0_sub Hop Hor) in Hzs23.
-          rewrite (rngl_mul_opp_r Hop).
-          rewrite (rngl_sub_opp_r Hop).
-          apply (rngl_mul_lt_mono_pos_l Hop Hor Hii (rngl_sin θ2) _ _ Hzs2).
-          eapply (rngl_le_lt_trans Hor); [ apply Hzs23 | ].
-          rewrite rngl_mul_add_distr_l.
-          do 2 rewrite rngl_mul_assoc.
-          rewrite fold_rngl_squ.
-          specialize (cos2_sin2_1 Hon Hop Hic Hed θ2) as H1.
-          apply (rngl_add_move_l Hop) in H1.
-          rewrite H1; clear H1.
-          rewrite (rngl_mul_sub_distr_r Hop).
-          rewrite (rngl_mul_1_l Hon).
-          rewrite (rngl_add_sub_assoc Hop).
-          apply (rngl_lt_add_lt_sub_r Hop Hor).
-          apply (rngl_lt_add_lt_sub_l Hop Hor).
-          apply (rngl_mul_le_mono_nonneg_l Hop Hor (rngl_cos θ2) _ _ Hzc2) in Hzs12.
-          rewrite rngl_mul_assoc in Hzs12.
-          rewrite fold_rngl_squ in Hzs12.
-          eapply (rngl_le_lt_trans Hor); [ apply Hzs12 | ].
-...
-          progress unfold rngl_squ.
-          rewrite <- rngl_mul_assoc.
-          rewrite <- rngl_mul_add_distr_l.
+            apply (rngl_le_lt_trans Hor) with
+              (a :=
+                 (rngl_sin θ3 * rngl_cos θ2 +
+                  rngl_cos θ2 * rngl_cos θ2 * rngl_sin θ1)%L)
+                in H. 2: {
+              apply (rngl_add_le_mono_l Hop Hor).
+              do 2 rewrite <- rngl_mul_assoc.
+              apply (rngl_mul_le_mono_pos_l Hop Hor Hii); [ easy | ].
+              now rewrite (rngl_mul_comm Hic (rngl_cos θ1)).
+            }
+            rewrite fold_rngl_squ in H.
+            apply (rngl_lt_add_lt_sub_r Hop Hor) in H.
+            rewrite (rngl_add_comm (rngl_sin θ1)) in H.
+            rewrite <- (rngl_add_sub_assoc Hop) in H.
+            rewrite (rngl_sub_mul_r_diag_r Hon Hop) in H.
+            specialize (cos2_sin2_1 Hon Hop Hic Hed θ2) as H1.
+            apply (rngl_add_move_l Hop) in H1.
+            rewrite <- H1 in H; clear H1.
+            rewrite (rngl_mul_comm Hic _²)%L in H.
+            eapply (rngl_le_lt_trans Hor) in H; [ | apply Hzs23 ].
+            progress unfold rngl_squ in H.
+            rewrite rngl_mul_assoc in H.
+            rewrite <- rngl_mul_add_distr_r in H.
+            apply (rngl_mul_lt_mono_pos_r Hop Hor Hii) in H; [ | easy ].
+            rewrite (rngl_mul_comm Hic (rngl_sin θ1)) in H.
+            cbn.
+            rewrite (rngl_mul_opp_r Hop).
+            now rewrite (rngl_sub_opp_r Hop).
+          }
+(* bon, ça serait en effet suffisant de prouver ça, mais c'est pas sûr que
+   ça soit vrai ; me voilà Gros-Jean comme devant *)
 ...
 
 Theorem angle_div_nat_is_inf_sum_of_angle_div_2_pow_nat :
