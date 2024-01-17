a4456b89f4cbc0f85ed09d0afd0b386532b48c25
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-01-17 13:00:24 +0100

    -

--- rngl_alg/Complex.v
+++ rngl_alg/Complex.v
@@ -5011,7 +5011,7 @@ split; intros H12. {
 }
 Qed.
 
-(* to be completed *)
+(* to be completed
 Theorem angle_div_nat_is_inf_sum_of_angle_div_2_pow_nat' :
   rngl_is_archimedean T = true →
   rngl_characteristic T = 0 →
