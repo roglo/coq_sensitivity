051bedfb56437778c596c2fd6f4ee32e599fc7c1
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-03-04 15:05:10 +0100

    -

--- rngl_alg/Work.v
+++ rngl_alg/Work.v
@@ -2233,30 +2233,82 @@ destruct zs. {
     specialize (H2 (angle_right / ₂) θ)%A.
     rewrite rngl_sin_right_div_2 in H2.
     rewrite rngl_cos_right_div_2 in H2.
-(* ouais, ça devrait le faire *)
+    specialize (H2 (rl_sqrt_half_pos Hc1) Hzs H1).
+    specialize (rl_sqrt_half_pos Hc1) as H.
+    apply rngl_ltb_lt in H.
+    rewrite H in H2; clear H.
+    now apply (rngl_lt_le_incl Hor) in H2.
+  } {
+    intros Hcs.
+    split; [ easy | ].
+    apply rngl_ltb_lt.
+    change_angle_add_r θ angle_straight.
+    progress sin_cos_add_sub_straight_hyp T Hcs.
+    progress sin_cos_add_sub_straight_hyp T Hzs.
+    progress sin_cos_add_sub_straight_goal T.
+    rewrite (rngl_add_opp_r Hop).
+    apply (rngl_lt_0_sub Hop Hor).
+    rewrite <- rngl_cos_right_div_2.
 ...
-  H1 : (√(1 / 2) < rngl_cos θ)%L
+    apply angle_add_le_mono_l_lemma_39. {
+      rewrite rngl_sin_right_div_2.
+      apply (rl_sqrt_half_pos Hc1).
+    } {
+      now apply (rngl_lt_le_incl Hor) in Hzs.
+    } {
+      rewrite rngl_cos_right_div_2.
+      apply rl_sqrt_half_nonneg.
+    } {
+      apply (rngl_lt_le_incl Hor) in Hcs, Hzs.
+      now apply (rngl_le_trans Hor _ (rngl_sin θ)).
+    } {
+...
+  cbn.
+  specialize (rngl_0_le_1 Hon Hop Hor) as H2.
+  apply rngl_leb_le in H2.
+  rewrite H2; clear H2.
+  rewrite (rngl_mul_1_l Hon).
+  rewrite rngl_add_0_r, (rngl_sub_0_r Hos).
+  rewrite (rngl_mul_opp_r Hop).
+  rewrite (rngl_add_opp_r Hop).
+  rewrite <- (rngl_mul_sub_distr_r Hop).
+  apply (rngl_mul_pos_pos Hop Hor Hii). {
+    now apply (rngl_lt_0_sub Hop Hor).
+  }
+  apply (rl_sqrt_half_pos Hc1).
+
+...
+  Hcs : (rngl_sin θ < rngl_cos θ)%L
   Hzs : (0 < rngl_sin θ)%L
   ============================
-  (rngl_sin θ < rngl_cos θ)%L
+  (√(1 / 2) < rngl_cos θ)%L
 ...
-    destruct (rngl_lt_dec Hor (rngl_cos θ) 0) as [Hzc| Hcz]. {
-      now apply (rngl_lt_le_trans Hor _ 0).
-    }
-    apply (rngl_nlt_ge Hor) in Hcz.
-    apply (rngl_lt_le_trans Hor _ √(1/2))%L; [ easy | ].
-    specialize rngl_sin_nonneg_cos_le_sin_le as H2.
-    specialize (H2 θ (angle_right / ₂) Hzs)%A.
+    apply (rngl_le_lt_trans Hor _ √(1/2))%L; [ | easy ].
+    specialize rngl_sin_nonneg_cos_lt_sin_lt as H2.
+    specialize (H2 (angle_right / ₂) θ)%A.
     rewrite rngl_sin_right_div_2 in H2.
     rewrite rngl_cos_right_div_2 in H2.
-    specialize (H2 rl_sqrt_half_nonneg).
-    generalize H1; intros H.
-    apply (rngl_lt_le_incl Hor) in H.
-    specialize (H2 H); clear H.
-    generalize Hcz; intros H.
-    apply rngl_leb_le in H.
-    now rewrite H in H2; clear H.
-  } {
+    specialize (H2 (rl_sqrt_half_pos Hc1) Hzs H1).
+    specialize (rl_sqrt_half_pos Hc1) as H.
+    apply rngl_ltb_lt in H.
+    rewrite H in H2; clear H.
+    now apply (rngl_lt_le_incl Hor) in H2.
+...
+apply angle_add_le_mono_l_lemma_39; try easy. {
+  apply (rngl_lt_iff Hor).
+  split; [ easy | ].
+  intros H; symmetry in H.
+  apply eq_rngl_sin_0 in H.
+  destruct H; subst θ. {
+    apply (rngl_nle_gt Hor) in Hcs.
+    apply Hcs; clear Hcs; cbn.
+    apply (rngl_0_le_1 Hon Hop Hor).
+  }
+  apply (rngl_nlt_ge Hor) in Hc.
+  apply Hc; clear Hc; cbn.
+  apply (rngl_opp_1_lt_0 Hon Hop Hor Hc1).
+} {
+  now cbn; rewrite (rngl_sub_0_r Hos).
 ...
 specialize rngl_sin_nonneg_cos_lt_sin_lt as H1.
 ...
