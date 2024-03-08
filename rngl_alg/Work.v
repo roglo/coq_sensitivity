5db9b7b7c235f419cba63d8497839d29bd2e54bd
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-03-08 20:34:26 +0100

    -

--- rngl_alg/Work.v
+++ rngl_alg/Work.v
@@ -1370,6 +1370,25 @@ rewrite angle_add_div_2_diag.
 apply angle_le_refl.
 Qed.
 
+Theorem angle_add_lt_mono_l :
+  ∀ θ1 θ2 θ3,
+  angle_add_overflow θ1 θ2 = false
+  → angle_add_overflow θ1 θ3 = false
+  → (θ2 < θ3)%A → (θ1 + θ2 < θ1 + θ3)%A.
+Proof.
+intros * H12 H13 H23.
+apply angle_lt_iff.
+split. {
+  apply angle_add_le_mono_l; [ easy | easy | ].
+  now apply angle_lt_le_incl in H23.
+}
+intros H.
+apply (f_equal (λ θ, (θ - θ1)%A)) in H.
+do 2 rewrite angle_add_comm, angle_add_sub in H.
+subst θ3.
+now apply angle_lt_irrefl in H23.
+Qed.
+
 (* to be completed
 Theorem angle_div_2_pow_mul_lt_angle :
   ∀ n i θ, θ ≠ 0%A → n < 2 ^ i → (n * (θ / ₂^i) < θ)%A.
@@ -1401,19 +1420,22 @@ assert (H1 : n = 2 ^ i + n mod 2 ^ i). {
   now rewrite Nat.mul_1_l.
 }
 rewrite H1.
-...
 rewrite angle_mul_add_distr_r.
 rewrite angle_div_2_pow_succ_r_2 at 1.
 rewrite angle_div_2_pow_mul_2_pow.
-apply angle_le_trans with (θ2 := (θ / ₂ + θ / ₂)%A). {
-  apply angle_add_le_mono_l; cycle 1. {
+apply angle_lt_le_trans with (θ2 := (θ / ₂ + θ / ₂)%A). {
+  apply angle_add_lt_mono_l; cycle 1. {
     apply angle_add_overflow_div_2_div_2.
   } {
     rewrite angle_div_2_pow_succ_r_2.
-    apply IHi.
-    apply Nat.mod_upper_bound.
-    now apply Nat.pow_nonzero.
+    apply IHi. {
+      apply Nat.mod_upper_bound.
+      now apply Nat.pow_nonzero.
+    }
+    intros H.
+    now apply eq_angle_div_2_0 in H.
   }
+...
   apply angle_add_overflow_le with (θ2 := (θ / ₂)%A). 2: {
     apply angle_add_overflow_div_2_div_2.
   }
