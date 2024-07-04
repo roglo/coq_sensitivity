13e82bf5df9309e60613b17830026b3c58d35a7d
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-07-04 04:56:05 +0200

    -

--- rngl_alg/Work2.v
+++ rngl_alg/Work2.v
@@ -1394,27 +1394,20 @@ destruct (lt_dec (2 ^ i) n) as [Hin| Hin]. {
   apply angle_lim_add_add_if in Hlim. 2: {
     intros ε Hε.
     specialize (rngl_archimedean Har Hor) as H1.
+(* ah oui, c'est casse-couilles, parce que ça boucle : je ne sais
+   pas quelle valeur choisir pour "a" dans H1; dois-je prendre ε ?
+   ou bien ε/n ? *)
+....
     set (ε' := (ε / rngl_of_nat n)%L).
     specialize (H1 ε' (angle_eucl_dist (θ / ₂^i) 0)).
-    assert (H : (0 < ε')%L) by admit.
+    assert (H : (0 < ε')%L) by ...
     specialize (H1 H); clear H.
     destruct H1 as (N, HN).
     rewrite (rngl_mul_nat_mul_nat_1 Hon Hos) in HN.
     subst ε'.
+    rewrite rngl_div_mul in HN.
 Search (_ / _ * _)%L.
-About rngl_mul_div_r.
 About rngl_div_mul.
-specialize rngl_mul_div_r as H1.
-specialize rngl_div_mul as H2.
-...
-rewrite rngl_div_mul 
-...
-rngl_mul_div_r:
-  ∀ (T : Type) (ro : ring_like_op T),
-    ring_like_prop T → rngl_has_1 T = true → rngl_has_inv T = true → ∀ a b : T, b ≠ 0%L → (a / b * b)%L = a
-rngl_div_mul:
-  ∀ (T : Type) (ro : ring_like_op T),
-    ring_like_prop T → rngl_has_1 T = true → rngl_has_inv T = true → ∀ a b : T, b ≠ 0%L → (a / b * b)%L = a
 ...
 Search (rngl_mul_nat (_ / _)).
 progress unfold rngl_mul_nat in H1.
