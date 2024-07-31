7dc6867b7237c041e36aa757e686eff856c2c51c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-07-31 09:29:36 +0200

    -

--- rngl_alg/Work2.v
+++ rngl_alg/Work2.v
@@ -2091,6 +2091,7 @@ destruct zs1. 2: {
   rewrite angle_sub_0_l in Hc211.
   rewrite rngl_cos_opp in Hc211.
   destruct (rngl_lt_dec Hor 0 (rngl_cos θ1)) as [Hzc1| Hzc1]. {
+(**)
     change_angle_add_r θ1 angle_right.
     progress sin_cos_add_sub_right_hyp T Hzs1.
     rewrite angle_sub_sub_distr in Hc211.
@@ -2101,6 +2102,16 @@ destruct zs1. 2: {
     progress sin_cos_add_sub_right_hyp T H12.
     progress sin_cos_add_sub_right_hyp T Hzc1.
     rewrite <- rngl_sin_sub_anticomm in Hc211, Hc213.
+(*
+    change_angle_opp θ1.
+    progress sin_cos_opp_hyp T Hzs1.
+    progress sin_cos_opp_hyp T Hc211.
+    rewrite angle_sub_opp_r in Hc211.
+    do 2 rewrite angle_sub_opp_r in Hc213.
+    progress sin_cos_opp_hyp T H13.
+    progress sin_cos_opp_hyp T H12.
+    progress sin_cos_opp_hyp T Hzc1.
+*)
     change_angle_add_r θ2 angle_right.
     rewrite angle_sub_sub_distr in Hc213, Hc211.
     progress sin_cos_add_sub_right_hyp T Hc213.
@@ -2108,6 +2119,15 @@ destruct zs1. 2: {
     progress sin_cos_add_sub_right_hyp T H12.
     progress sin_cos_add_sub_right_hyp T Hzs2.
     progress sin_cos_add_sub_right_goal T.
+(*
+    change_angle_opp θ2.
+    rewrite angle_sub_opp_r in Hc213.
+    rewrite angle_sub_opp_r in Hc211.
+    progress sin_cos_opp_hyp T H12.
+    progress sin_cos_opp_hyp T Hzs2.
+    rewrite rngl_cos_opp.
+
+*)
     change_angle_add_r θ3 angle_right.
     rewrite angle_sub_sub_swap in Hc213.
     progress sin_cos_add_sub_right_hyp T Hc213.
@@ -2119,6 +2139,7 @@ destruct zs1. 2: {
     move Hzc1 before Hzs1.
     move H12 before Hzc1; move Hzs2 before H12.
     move H13 before Hzs2; move Hzs3 before H13.
+...
     rewrite rngl_cos_sub in Hc211.
     do 2 rewrite rngl_cos_sub in Hc213.
 (* mouais bof *)
