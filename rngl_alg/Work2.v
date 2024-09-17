ec58b97297bb0a3ffda17064b83e680d08782eed
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-09-17 09:46:55 +0200

    -

--- rngl_alg/Work2.v
+++ rngl_alg/Work2.v
@@ -5739,9 +5739,9 @@ destruct (rngl_le_dec Hor (rngl_cos (θ q)) (rngl_cos (θ p))) as [Hpq| Hpq]. {
 }
 Qed.
 
-(* essai de faire une espèce d'anneau des angles, en définissant une
-   multiplication bidon qui renvoie toujours 0 *)
 (* to be completed
+(* RingLike on angles by defining a phony multiplication always
+   returning 0 *)
 
 Definition angle_phony_mul (θ1 θ2 : angle T) := 0%A.
 
@@ -5753,68 +5753,135 @@ Definition angle_ring_like_op : ring_like_op (angle T) :=
      rngl_opt_opp_or_subt := Some (inl angle_opp);
      rngl_opt_inv_or_quot := None;
      rngl_opt_eq_dec := Some (angle_eq_dec);
-     rngl_opt_leb := None |}.
+     rngl_opt_leb := Some (angle_leb) |}.
 
 (*
 Canonical Structure angle_ring_like_op.
 *)
 
 Theorem rngl_angle_add_comm :
-  let rom := angle_ring_like_op in
-  ∀ (θ1 θ2 : angle T), (θ1 + θ2)%L = (θ2 + θ1)%L.
+  let roa := angle_ring_like_op in
+  ∀ (a b : angle T), (a + b)%L = (b + a)%L.
+Proof. apply angle_add_comm. Qed.
+
+Theorem rngl_angle_add_assoc :
+  let roa := angle_ring_like_op in
+  ∀ a b c : angle T, (a + (b + c))%L = (a + b + c)%L.
+Proof. apply angle_add_assoc. Qed.
+
+Theorem rngl_angle_add_0_l :
+  let roa := angle_ring_like_op in
+  ∀ a : angle T, (0 + a)%L = a.
+Proof. apply angle_add_0_l. Qed.
+
+Theorem rngl_angle_mul_assoc :
+  let roa := angle_ring_like_op in
+  ∀ a b c : angle T, (a * (b * c))%L = (a * b * c)%L.
+Proof. easy. Qed.
+
+Theorem rngl_angle_mul_add_distr_l :
+  let roa := angle_ring_like_op in
+  ∀ a b c : angle T, (a * (b + c))%L = (a * b + a * c)%L.
 Proof.
-apply angle_add_comm.
+intros; cbn; progress unfold angle_phony_mul.
+symmetry; apply angle_add_0_l.
 Qed.
 
-Theorem angle_opt_le_dec : True. easy. Qed.
-Theorem angle_opt_integral : True. easy. Qed.
-Theorem angle_opt_characteristic_prop : True. easy. Qed.
-Theorem angle_opt_le_refl : True. easy. Qed.
-Theorem angle_opt_le_antisymm : True. easy. Qed.
-Theorem angle_opt_le_trans : True. easy. Qed.
-Theorem angle_opt_add_le_compat : True. easy. Qed.
-Theorem angle_opt_mul_le_compat_nonneg : True. easy. Qed.
-Theorem angle_opt_mul_le_compat_nonpos : True. easy. Qed.
-Theorem angle_opt_not_le : True. easy. Qed.
-Theorem angle_opt_archimedean : True. easy. Qed.
+Theorem rngl_angle_opt_mul_comm :
+  let roa := angle_ring_like_op in
+  ∀ a b : angle T, (a * b)%L = (b * a)%L.
+Proof. easy. Qed.
+
+Theorem rngl_angle_opt_add_opp_diag_l :
+  let roa := angle_ring_like_op in
+  if rngl_has_opp (angle T) then ∀ a : angle T, (- a + a)%L = 0%L
+  else not_applicable.
+Proof. cbn; apply angle_add_opp_diag_l. Qed.
+
+Theorem rngl_angle_opt_le_dec :
+  let roa := angle_ring_like_op in
+  if rngl_is_ordered (angle T) then
+    ∀ a b : angle T, {(a ≤ b)%L} + {¬ (a ≤ b)%L}
+  else not_applicable.
+Proof. intros; cbn; apply angle_le_dec. Qed.
+
+Theorem rngl_angle_opt_le_refl :
+  let roa := angle_ring_like_op in
+  if rngl_is_ordered (angle T) then ∀ a : angle T, (a ≤ a)%L
+  else not_applicable.
+Proof. intros; cbn; apply angle_le_refl. Qed.
+
+Theorem rngl_angle_opt_le_antisymm :
+  let roa := angle_ring_like_op in
+  if rngl_is_ordered (angle T) then
+    ∀ a b : angle T, (a ≤ b)%L → (b ≤ a)%L → a = b
+  else not_applicable.
+Proof. cbn; apply angle_le_antisymm. Qed.
+
+Theorem rngl_angle_opt_le_trans :
+  let roa := angle_ring_like_op in
+  if rngl_is_ordered (angle T) then
+    ∀ a b c : angle T, (a ≤ b)%L → (b ≤ c)%L → (a ≤ c)%L
+  else not_applicable.
+Proof. cbn; apply angle_le_trans. Qed.
+
+Theorem rngl_angle_opt_add_le_compat :
+  let roa := angle_ring_like_op in
+  if rngl_is_ordered (angle T) then
+    ∀ a b c d : angle T, (a ≤ b)%L → (c ≤ d)%L → (a + c ≤ b + d)%L
+  else not_applicable.
+Proof.
+cbn; intros.
+apply (angle_le_trans _ (b + c)). {
+  apply angle_add_le_mono_r; [ | easy ].
+(* ah ouais, ça déconne, là ; il faut que b + c ne déborde pas
+  ============================
+  angle_add_overflow b c = false
+*)
+...
+
+Theorem rngl_angle_opt_mul_le_compat_nonneg : True. easy. Qed.
+Theorem rngl_angle_opt_mul_le_compat_nonpos : True. easy. Qed.
+Theorem rngl_angle_opt_not_le : True. easy. Qed.
+Theorem rngl_angle_opt_archimedean : True. easy. Qed.
 
 Definition angle_ring_like_prop :
   let rom := angle_ring_like_op in
   ring_like_prop (angle T) :=
   {| rngl_mul_is_comm := true;
-     rngl_is_integral_domain := true;
+     rngl_is_integral_domain := false;
      rngl_is_archimedean := true;
      rngl_is_alg_closed := false;
      rngl_characteristic := 0;
      rngl_add_comm := rngl_angle_add_comm;
-     rngl_add_assoc := ...
-     rngl_add_0_l := Z.add_0_l;
-     rngl_mul_assoc := Z.mul_assoc;
-     rngl_opt_mul_1_l := Z.mul_1_l;
-     rngl_mul_add_distr_l := Z.mul_add_distr_l;
-     rngl_opt_mul_comm := Z.mul_comm;
+     rngl_add_assoc := rngl_angle_add_assoc;
+     rngl_add_0_l := rngl_angle_add_0_l;
+     rngl_mul_assoc := rngl_angle_mul_assoc;
+     rngl_opt_mul_1_l := NA;
+     rngl_mul_add_distr_l := rngl_angle_mul_add_distr_l;
+     rngl_opt_mul_comm := rngl_angle_opt_mul_comm;
      rngl_opt_mul_1_r := NA;
      rngl_opt_mul_add_distr_r := NA;
-     rngl_opt_add_opp_diag_l := Z.add_opp_diag_l;
+     rngl_opt_add_opp_diag_l := rngl_angle_opt_add_opp_diag_l;
      rngl_opt_add_sub := NA;
      rngl_opt_sub_add_distr := NA;
      rngl_opt_mul_inv_diag_l := NA;
      rngl_opt_mul_inv_diag_r := NA;
-     rngl_opt_mul_div := Z.quot_mul;
+     rngl_opt_mul_div := NA;
      rngl_opt_mul_quot_r := NA;
-     rngl_opt_le_dec := angle_opt_le_dec;
-     rngl_opt_integral := angle_opt_integral;
+     rngl_opt_le_dec := rngl_angle_opt_le_dec;
+     rngl_opt_integral := NA;
      rngl_opt_alg_closed := NA;
-     rngl_opt_characteristic_prop := angle_opt_characteristic_prop;
-     rngl_opt_le_refl := angle_opt_le_refl;
-     rngl_opt_le_antisymm := angle_opt_le_antisymm;
-     rngl_opt_le_trans := angle_opt_le_trans;
-     rngl_opt_add_le_compat := angle_opt_add_le_compat;
-     rngl_opt_mul_le_compat_nonneg := angle_opt_mul_le_compat_nonneg;
-     rngl_opt_mul_le_compat_nonpos := angle_opt_mul_le_compat_nonpos;
+     rngl_opt_characteristic_prop := NA;
+     rngl_opt_le_refl := rngl_angle_opt_le_refl;
+     rngl_opt_le_antisymm := rngl_angle_opt_le_antisymm;
+     rngl_opt_le_trans := rngl_angle_opt_le_trans;
+     rngl_opt_add_le_compat := rngl_angle_opt_add_le_compat;
+     rngl_opt_mul_le_compat_nonneg := rngl_angle_opt_mul_le_compat_nonneg;
+     rngl_opt_mul_le_compat_nonpos := rngl_angle_opt_mul_le_compat_nonpos;
      rngl_opt_mul_le_compat_non_opp := NA;
-     rngl_opt_not_le := angle_opt_not_le;
-     rngl_opt_archimedean := angle_opt_archimedean |}.
+     rngl_opt_not_le := rngl_angle_opt_not_le;
+     rngl_opt_archimedean := rngl_angle_opt_archimedean |}.
 *)
 
 (* to be completed
