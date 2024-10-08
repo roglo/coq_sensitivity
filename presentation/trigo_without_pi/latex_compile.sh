3dfd0b695fd8251c928eaff3a67fb4d467eeb541
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-10-08 16:39:04 +0200

    -

--- presentation/trigo_without_pi/latex_compile.sh
+++ presentation/trigo_without_pi/latex_compile.sh
@@ -4,6 +4,7 @@ latexmk -pdf -file-line-error -halt-on-error "$1" > latex.log 2>&1
 
 if [ $? -ne 0 ]; then
     sed -n -e "/^.\/$1:[1-9]/,/Transcript written/p" latex.log
+    sed -n -e "/^Runaway argument/,/Transcript written/p" latex.log
     rm -f *.fdb_latexmk
     exit 1
 else
