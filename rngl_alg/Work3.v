787f2172b62b4b13c5a4702a25a241c2cec6a500
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-10-15 11:37:26 +0200

    -

--- rngl_alg/Work3.v
+++ rngl_alg/Work3.v
@@ -7,6 +7,7 @@ Import List.ListNotations.
 
 Require Import Main.Misc Main.RingLike.
 Require Import Main.IterMax.
+Require Import Main.IterAdd.
 Require Import Trigo.RealLike.
 Require Import Trigo.TrigoWithoutPi Trigo.TrigoWithoutPiExt.
 Require Import Trigo.AngleAddLeMonoL.
@@ -282,6 +283,12 @@ assert (Hpz :
   now apply -> Nat.succ_lt_mono.
 }
 clear H1.
+rename n into m; rename Hn into Hm.
+remember (m - 1) as n eqn:Hn.
+replace (m - 2) with (n - 1) in Hr by flia Hn.
+assert (H1 :
+  (‖ P.[n] ‖ - ∑ (k = 0, n - 1), ‖ P.[k] * z ^ k ‖ ≤
+   ‖ rngl_eval_polyn P z ‖)%L). {
 ...
 *)
 
