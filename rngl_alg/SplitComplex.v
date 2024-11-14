b0bc59df8456170c46a827b61b6df192e624c5a3
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-11-14 14:14:00 +0100

    -

--- rngl_alg/SplitComplex.v
+++ rngl_alg/SplitComplex.v
@@ -13,36 +13,17 @@ Require Import Misc.
      k²=1  : split such that k=matrix (0 1) (0 -1)
 *)
 
-Class SplitComplex T := mk_sc {gre : T; sc1 : T; sc2 : T; sc3 : T}.
+Class SplitComplex T := mk_sc {sre : T; sc1 : T; sc2 : T; sc3 : T}.
 
 Declare Scope sc_scope.
 Delimit Scope sc_scope with S.
 Bind Scope sc_scope with SplitComplex.
 
-Arguments mk_sc {T} (sc1 sc2 sc3)%_L : rename.
+Arguments mk_sc {T} (sre sc1 sc2 sc3)%_L : rename.
+Arguments sre {T} a%_L : rename.
 Arguments sc1 {T} a%_L : rename.
 Arguments sc2 {T} a%_L : rename.
 Arguments sc3 {T} a%_L : rename.
-
-...
-
-(* I'd like to use them with U={(i,j,k)} such that
-     i²=-1 : complex
-     j²=1  : split such that j=matrix (0 1) (1 0)
-     k²=1  : split such that k=matrix (0 1) (0 -1)
-  but is it a "ring-like" by itself?
-*)
-
-Declare Scope cs_scope.
-Delimit Scope cs_scope with S.
-Bind Scope cs_scope with ComplexSplit.
-
-Class ComplexSplit T := mk_cs {cim : T; cs1 : T; cs2 : T}.
-
-Arguments mk_cs {T} (cim cs1 cs2)%_L.
-Arguments cim {T} a%_L : rename.
-Arguments cs1 {T} a%_L : rename.
-Arguments cs2 {T} a%_L : rename.
 
 Section a.
 
@@ -50,20 +31,22 @@ Context {T : Type}.
 Context {ro : ring_like_op T}.
 Context {rp : ring_like_prop T}.
 
-Definition cs_zero := mk_cs 0 0 0.
+Definition sc_zero := mk_sc 0 0 0 0.
 
-Definition cs_add a b :=
-  mk_cs (cim a + cim b) (cs1 a + cs1 b) (cs2 a + cs2 b).
+Definition sc_add a b :=
+  mk_sc (sre a + sre b) (sc1 a + sc1 b) (sc2 a + sc2 b) (sc2 a + sc2 b).
 
-Definition cs_mul a b :=
-... ah oui non c'est déjà foutu ...
+Definition sc_mul a b :=
+  mk_sc (sre a * sre b - sc1 a * sc1 b + ...)
 
-Instance cs_ring_like_op T
+...
+
+Instance sc_ring_like_op T
   {ro : ring_like_op T} {rp : ring_like_prop T} :
-  ring_like_op (ComplexSplit T) :=
-  {| rngl_zero := cs_zero;
-     rngl_add := cs_add;
-     rngl_mul := cs_mul;
+  ring_like_op (SplitComplex T) :=
+  {| rngl_zero := sc_zero;
+     rngl_add := sc_add;
+     rngl_mul := sc_mul;
      rngl_opt_one := gc_opt_one;
      rngl_opt_opp_or_subt := gc_opt_opp_or_subt T;
      rngl_opt_inv_or_quot := gc_opt_inv_or_quot T;
