3bf65a4050a7b87649f1e8e3b6f4594a8a2b4618
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-11-24 14:09:14 +0100

    -

--- rngl_alg/HyperbolicTrigo.v
+++ rngl_alg/HyperbolicTrigo.v
@@ -1918,6 +1918,20 @@ Definition rngl_expc (θ : angle T) := rngl_exph (hangle_of_angle θ).
 Theorem fold_rl_modl : ∀ x y, √(x² + y²) = rl_modl x y.
 Proof. easy. Qed.
 
+(* to be moved to the right place *)
+Theorem angle_neqb_neq :
+  ∀ θ1 θ2, (θ1 ≠? θ2)%A = true ↔ θ1 ≠ θ2.
+Proof.
+intros.
+split; intros H12. {
+  apply angle_eqb_neq.
+  now apply Bool.negb_true_iff.
+} {
+  apply Bool.negb_true_iff.
+  now apply angle_eqb_neq.
+}
+Qed.
+
 (* to be completed
 
 Theorem rngl_cos_derivative :
@@ -1976,69 +1990,11 @@ Theorem rngl_cos_add_cos :
   (rngl_cos p + rngl_cos q =
      2 * rngl_sin ((p + q) /₂) * rngl_sin ((p - q) /₂))%L.
 Proof.
-Admitted.
-
-Theorem rngl_cos_sub_cos :
-  ∀ p q,
-  (rngl_cos p - rngl_cos q =
-     - (2 * rngl_sin ((p + q) /₂) * rngl_sin ((p - q) /₂)))%L.
-Proof.
-destruct_ac.
-intros.
-specialize (rngl_cos_add_cos p (q + angle_straight)) as H1.
-rewrite angle_add_assoc in H1.
-rewrite angle_sub_add_distr in H1.
-rewrite rngl_cos_add_straight_r in H1.
-rewrite (rngl_add_opp_r Hop) in H1.
-(* I need a lemma angle_lt_dec *)
-remember ((p + q) <? angle_straight)%A as pqs eqn:Hpqs.
-symmetry in Hpqs.
-destruct pqs. {
-  rewrite rngl_sin_angle_div_2_add_not_overflow in H1. 2: {
-    (* lemma *)
-    progress unfold angle_add_overflow.
-    apply Bool.andb_false_iff.
-    remember (p + q ≠? 0)%A as pqz eqn:Hpqz.
-    symmetry in Hpqz.
-    destruct pqz; [ right | now left ].
-(* ça va pas, mon <? et mon ≤? dans les angles
-   faut que je corrige ça d'abord *)
-    apply Bool.not_true_iff_false.
-    intros H.
-...
-    (* lemma *)
-    rewrite <- angle_add_overflow_equiv3.
-    progress unfold old_angle_add_overflow.
-    (* lemma *)
-    apply Bool.not_true_iff_false.
-    intros H.
-    apply angle_nle_gt in H.
-    apply H; clear H.
-    (* end lemma *)
-Search (_ ≤ _ + _)%A.
-    (* lemma *)
-    rewrite <- (angle_add_0_r (p + q)) at 1.
-    apply angle_add_le_mono_l.
-...
-    apply angle_le_add_l.
-    apply angle_ltb_ge.
-...
-    rewrite angle_add_overflow_equiv2.
-    progress unfold angle_add_overflow.
-    apply Bool.andb_false_iff.
-
-Search (_ → angle_add_overflow _ _ = false).
-Search (rngl_sin ((_ + _) /₂)).
-...
-rewrite H1; clear H1.
-...
-
-(* for rngl_cos_add_cos *)
 destruct_ac.
 destruct (Nat.eq_dec (rngl_characteristic T) 1) as [Hc1| Hc1]. {
   specialize (rngl_characteristic_1 Hon Hos Hc1) as H1.
   intros.
-  rewrite (H1 (- _)%L).
+  rewrite (H1 (_ * _)%L).
   apply H1.
 }
 intros.
@@ -2094,6 +2050,62 @@ do 2 rewrite (rngl_mul_sub_distr_r Hop).
 do 2 rewrite (rngl_mul_1_r Hon).
 rewrite (rngl_mul_1_l Hon).
 cbn.
+...
+
+Theorem rngl_cos_sub_cos :
+  ∀ p q,
+  (rngl_cos p - rngl_cos q =
+     - (2 * rngl_sin ((p + q) /₂) * rngl_sin ((p - q) /₂)))%L.
+Proof.
+destruct_ac.
+intros.
+specialize (rngl_cos_add_cos p (q + angle_straight)) as H1.
+rewrite angle_add_assoc in H1.
+rewrite angle_sub_add_distr in H1.
+rewrite rngl_cos_add_straight_r in H1.
+rewrite (rngl_add_opp_r Hop) in H1.
+(* I need a lemma angle_lt_dec *)
+remember ((p + q) <? angle_straight)%A as pqs eqn:Hpqs.
+symmetry in Hpqs.
+destruct pqs. {
+  rewrite rngl_sin_angle_div_2_add_not_overflow in H1. 2: {
+    (* lemma *)
+    progress unfold angle_add_overflow.
+    apply Bool.andb_false_iff.
+    remember (p + q ≠? 0)%A as pqz eqn:Hpqz.
+    symmetry in Hpqz.
+    destruct pqz; [ right | now left ].
+    apply angle_leb_gt.
+    apply angle_lt_opp_r; [ | now rewrite angle_opp_straight ].
+    intros H.
+    now apply angle_neqb_neq in Hpqz.
+  }
+...
+    (* lemma *)
+    rewrite <- angle_add_overflow_equiv3.
+    progress unfold old_angle_add_overflow.
+    (* lemma *)
+    apply Bool.not_true_iff_false.
+    intros H.
+    apply angle_nle_gt in H.
+    apply H; clear H.
+    (* end lemma *)
+Search (_ ≤ _ + _)%A.
+    (* lemma *)
+    rewrite <- (angle_add_0_r (p + q)) at 1.
+    apply angle_add_le_mono_l.
+...
+    apply angle_le_add_l.
+    apply angle_ltb_ge.
+...
+    rewrite angle_add_overflow_equiv2.
+    progress unfold angle_add_overflow.
+    apply Bool.andb_false_iff.
+
+Search (_ → angle_add_overflow _ _ = false).
+Search (rngl_sin ((_ + _) /₂)).
+...
+rewrite H1; clear H1.
 ...
 rewrite rngl_mul_assoc.
 ...
