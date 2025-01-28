a84cf5184483bc950878bfa975caa674ddc95717
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-01-28 16:59:49 +0100

    -

--- rngl_alg/AngleDeriv.v
+++ rngl_alg/AngleDeriv.v
@@ -1631,7 +1631,7 @@ split. {
     now apply angle_eucl_dist_separation in H.
   }
 }
-intros θ (Htt, Hovt) H1.
+intros θ (Hlt, Htt) H1.
 move θ before θ₀.
 apply (rngl_min_glb_lt_iff Hor) in H1.
 destruct H1 as (H1, H3).
@@ -1650,22 +1650,36 @@ rewrite rngl_mul_assoc.
 rewrite (rngl_mul_div Hi1). 2: {
   intros H.
   apply angle_eucl_dist_separation in H.
-  rewrite H in Htt.
-  now apply angle_lt_irrefl in Htt.
+  rewrite H in Hlt.
+  now apply angle_lt_irrefl in Hlt.
 }
+(*
 rewrite angle_add_overflow_comm in Hovt.
 rewrite Hovt.
+*)
 rewrite <- (rngl_abs_opp Hop Hor).
 rewrite (rngl_opp_add_distr Hop).
 rewrite (rngl_sub_opp_r Hop).
 rewrite (rngl_add_opp_l Hop).
-generalize Htt; intros H.
+generalize Hlt; intros H.
 apply angle_lt_le_incl in H.
 apply angle_nlt_ge in H.
 apply Bool.not_true_iff_false in H.
 rewrite H; clear H.
 rewrite (rngl_mul_1_r Hon).
-rewrite angle_add_0_r.
+(**)
+rewrite angle_div_2_add.
+replace (rngl_abs _) with
+  (rngl_abs (rngl_sin (θ /₂ + θ₀ /₂) - rngl_sin θ₀)). 2: {
+  remember (angle_add_overflow θ θ₀) as ovt eqn:Hovt.
+  symmetry in Hovt.
+  destruct ovt. {
+    rewrite <- angle_add_assoc.
+    rewrite angle_straight_add_straight.
+    now rewrite angle_add_0_r.
+  }
+  now rewrite angle_add_0_r.
+}
 apply Hsc.
 eapply (rngl_le_lt_trans Hor); [ | apply H3 ].
 clear η Hη Hsc H3.
@@ -1673,11 +1687,12 @@ rewrite angle_eucl_dist_move_0_r.
 rewrite (angle_eucl_dist_move_0_r θ).
 rewrite <- (angle_div_2_mul_2 θ₀) at 2.
 rewrite angle_mul_nat_div_2. 2: {
-  clear - Hor Hop H1 H2 Hovt Htt Hts Htz.
+  clear - Hor Hop H1 H2 Htt Hts Htz.
   cbn.
   rewrite angle_add_0_r.
   rewrite Bool.orb_false_r.
   apply angle_lt_straight_add_same_not_overflow.
+...
   apply (angle_lt_trans _ θ); [ easy | ].
   apply rngl_cos_lt_iff_angle_eucl_lt in H1, H2.
   rewrite angle_sub_0_r in H1.
