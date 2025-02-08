8f2e4d7e915600bf3092b391d4526e2b7e56c67b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-02-08 18:22:13 +0100

    -

--- rngl_alg/AngleTan.v
+++ rngl_alg/AngleTan.v
@@ -15,17 +15,61 @@ Definition rngl_tan θ := (rngl_sin θ / rngl_cos θ)%L.
 Context {Hop : rngl_has_opp T = true}.
 Context {Hor : rngl_is_ordered T = true}.
 
-(* to be completed
+Theorem rngl_dist_mul_distr_r :
+ (rngl_is_integral_domain T || rngl_has_inv_and_1_or_quot T)%bool = true →
+  ∀ a b c,
+  (0 ≤ c)%L → (rngl_dist a b * c = rngl_dist (a * c) (b * c))%L.
+Proof.
+specialize (rngl_has_opp_has_opp_or_subt Hop) as Hos.
+intros Hii.
+intros * Hzc.
+progress unfold rngl_dist.
+rewrite <- (rngl_mul_sub_distr_r Hop).
+progress unfold rngl_abs.
+rewrite (rngl_mul_sub_distr_r Hop).
+do 2 rewrite (rngl_leb_sub_0 Hop Hor).
+rewrite <- (rngl_mul_sub_distr_r Hop).
+remember (a ≤? b)%L as ab eqn:Hab.
+remember (a * c ≤? b * c)%L as acbc eqn:Hacbc.
+symmetry in Hab, Hacbc.
+destruct ab. {
+  destruct acbc; [ apply (rngl_mul_opp_l Hop) | ].
+  apply rngl_leb_le in Hab.
+  apply (rngl_leb_gt Hor) in Hacbc.
+  exfalso.
+  apply rngl_nle_gt in Hacbc.
+  apply Hacbc; clear Hacbc.
+  now apply (rngl_mul_le_mono_nonneg_r Hop Hor).
+}
+apply (rngl_leb_gt Hor) in Hab.
+destruct acbc; [ | easy ].
+apply rngl_leb_le in Hacbc.
+apply (rngl_lt_eq_cases Hor) in Hzc.
+destruct Hzc as [Hzc| Hzc]. {
+  exfalso.
+  apply rngl_nlt_ge in Hacbc.
+  apply Hacbc; clear Hacbc.
+  now apply (rngl_mul_lt_mono_pos_r Hop Hor Hii).
+}
+subst c.
+rewrite (rngl_mul_0_r Hos).
+symmetry.
+apply (rngl_opp_0 Hop).
+Qed.
+
 Definition rngl_distance :=
   {| d_dist := rngl_dist; d_prop := rngl_dist_is_dist Hop Hor |}.
 
+(* to be completed
 Theorem left_derivative_mul :
+ (rngl_is_integral_domain T || rngl_has_inv_and_1_or_quot T)%bool = true →
   ∀ A lt da (f g f' g' : A → T) x₀,
   left_derivative_at lt da rngl_distance f f' x₀
   → left_derivative_at lt da rngl_distance g g' x₀
   → left_derivative_at lt da rngl_distance (λ x : A, (f x * g x)%L)
       (λ x : A, (f x * g' x + f' x * g x)%L) x₀.
 Proof.
+intros Hii.
 intros * Hf Hg.
 intros ε Hε.
 specialize (Hf ε Hε).
@@ -51,13 +95,18 @@ assert (H : (d_dist x x₀ < ηg)%L). {
   apply (rngl_le_min_r Hor).
 }
 specialize (Hg H); clear H.
-Theorem rngl_dist_mul_distr_r :
-  ∀ a b c, (rngl_dist a b * c = rngl_dist (a * c) (b * c))%L.
-Proof.
-intros.
-progress unfold rngl_dist.
-rewrite <- (rngl_mul_sub_distr_r Hop).
-progress unfold rngl_abs.
+apply (rngl_mul_lt_mono_pos_r Hop Hor Hii (d_dist x x₀)) in Hf.
+cbn in Hf.
+rewrite (rngl_dist_mul_distr_r Hii) in Hf. 2: {
+  destruct da as (da, dap).
+  cbn.
+Search (0 ≤ angle_eucl_dist _ _)%L.
+Require Import Trigo.TrigoWithoutPiExt.
+Check angle_eucl_dist_nonneg.
+...
+Search rngl_dist.
+Search (0 ≤ rngl_dist _ _)%L.
+Search (0 ≤ d_dist _ _)%L.
 ....
 
 Theorem derivative_mul :
