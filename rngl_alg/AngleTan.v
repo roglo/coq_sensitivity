3c685696d4a17db0f170833c20997112221634df
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-02-08 16:20:42 +0100

    -

--- rngl_alg/AngleTan.v
+++ rngl_alg/AngleTan.v
@@ -6,19 +6,45 @@ Section a.
 
 Context {T : Type}.
 Context {ro : ring_like_op T}.
+Context {rp : ring_like_prop T}.
 
 Definition rngl_tan θ := (rngl_sin θ / rngl_cos θ)%L.
 
 (* to be completed
 Theorem left_derivative_mul :
+  rngl_is_ordered T = true →
   ∀ A lt da db (f g f' g' : A → T) x₀,
   left_derivative_at lt da db f f' x₀
   → left_derivative_at lt da db g g' x₀
   → left_derivative_at lt da db (λ x : A, (f x * g x)%L)
       (λ x : A, (f x * g' x + f' x * g x)%L) x₀.
 Proof.
+intros Hor.
 intros * Hf Hg.
 intros ε Hε.
+specialize (Hf ε Hε).
+specialize (Hg ε Hε).
+move Hε before ε.
+destruct Hf as (ηf & Hηf & Hf).
+destruct Hg as (ηg & Hηg & Hg).
+move ηf before ε.
+move ηg before ηf.
+move Hηg before Hηf.
+exists (rngl_min ηf ηg).
+split; [ now apply rngl_min_glb_lt | ].
+intros x Hlt Hd.
+specialize (Hf x Hlt).
+specialize (Hg x Hlt).
+assert (H : (d_dist x x₀ < ηf)%L). {
+  eapply (rngl_lt_le_trans Hor); [ apply Hd | ].
+  apply (rngl_le_min_l Hor).
+}
+specialize (Hf H); clear H.
+assert (H : (d_dist x x₀ < ηg)%L). {
+  eapply (rngl_lt_le_trans Hor); [ apply Hd | ].
+  apply (rngl_le_min_r Hor).
+}
+specialize (Hg H); clear H.
 ....
 
 Theorem derivative_mul :
