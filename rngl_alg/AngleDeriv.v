bff7eeae09750316a15b13280949aaa731a104b7
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-03-07 11:50:50 +0100

    -

--- rngl_alg/AngleDeriv.v
+++ rngl_alg/AngleDeriv.v
@@ -1709,10 +1709,8 @@ Theorem rngl_cos_derivative :
     rngl_cos (rngl_opp ° rngl_sin)%L.
 Proof.
 intros θ₀.
-split.
-apply rngl_cos_is_continuous.
-split.
-apply rngl_cos_is_continuous.
+split; [ apply rngl_cos_is_continuous | ].
+split; [ apply rngl_cos_is_continuous | ].
 split.
 apply rngl_cos_left_derivative.
 apply rngl_cos_right_derivative.
@@ -1723,10 +1721,8 @@ Theorem rngl_sin_derivative :
     rngl_sin rngl_cos.
 Proof.
 intros θ₀.
-split.
-apply rngl_sin_is_continuous.
-split.
-apply rngl_sin_is_continuous.
+split; [ apply rngl_sin_is_continuous | ].
+split; [ apply rngl_sin_is_continuous | ].
 split.
 apply rngl_sin_left_or_right_derivative.
 apply rngl_sin_left_or_right_derivative.
