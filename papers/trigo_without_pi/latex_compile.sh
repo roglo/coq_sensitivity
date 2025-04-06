71a2ddc1fb4a1075106a8f5da5230c7e27552553
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-04-06 03:13:19 +0200

    Added beginning of paper on trigonometry without pi.

---
+++ papers/trigo_without_pi/latex_compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+latexmk -pdf -file-line-error -halt-on-error "$1" > latex.log 2>&1
+
+if [ $? -ne 0 ]; then
+    sed -n -e "/^.\/$1:[1-9]/,/Transcript written/p" latex.log
+    sed -n -e "/^Runaway argument/,/Transcript written/p" latex.log
+    rm -f *.fdb_latexmk
+    exit 1
+else
+    exit 0
+fi
