b7090703d5c4e6ef1fbf5487f98e03c8aab1f718
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-04-06 05:16:31 +0200

    -

--- papers/trigo_without_pi/latex_compile.sh
+++ papers/trigo_without_pi/latex_compile.sh
@@ -3,6 +3,7 @@
 latexmk -pdf -file-line-error -halt-on-error "$1" > latex.log 2>&1
 
 if [ $? -ne 0 ]; then
+    grep "^./main.tex:" latex.log
     sed -n -e "/^.\/$1:[1-9]/,/Transcript written/p" latex.log
     sed -n -e "/^Runaway argument/,/Transcript written/p" latex.log
     rm -f *.fdb_latexmk
