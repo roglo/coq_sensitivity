5d63445ae33d50796d648d6394a9a542f26589eb
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-05-02 11:55:08 +0200

    -

--- rngl_alg/Quaternion.v
+++ rngl_alg/Quaternion.v
@@ -150,7 +150,7 @@ Notation "U ∗" := (nion_conj U) (at level 1, format "U ∗") : H_scope.
 Notation "U * V" := (nion_mul U V) : H_scope.
 
 Require Import ZArith.
-Require Import Zrl.
+Require Import RingLike.ZRingLike.
 Open Scope Z_scope.
 
 (* trinions: i²=-1 j²=-1 ij=0 *)
